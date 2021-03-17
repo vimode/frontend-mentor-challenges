4e13fe19059837f3795fbe58395bcbb15796542a
ppusher <thelastjediwarrior-cs50@yahoo.com>
2021-03-17 19:55:14 +0530

    code cleanup

--- faq-accordion-card/script.js
+++ faq-accordion-card/script.js
@@ -1,13 +1,11 @@
 let question = document.querySelectorAll(".single_question");
 
-question.forEach((item) => item.addEventListener("click", ()=>{
+question.forEach((item) => item.addEventListener("click", () => {
 
-  if(item.parentNode.classList.contains("active")) {
-    item.parentNode.classList.toggle("active")
-  } else {
+  if ( item.parentNode.classList.contains("active") ) {
+      item.parentNode.classList.toggle("active")
+    } else {
     question.forEach(item => item.parentNode.classList.remove("active"));
     item.parentNode.classList.add("active");
   }
-}))
-
-
+  }))
