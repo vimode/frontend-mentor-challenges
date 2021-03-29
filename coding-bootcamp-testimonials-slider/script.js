0d45f10a9e0ccaf6901e802be74dab34c35e1ff9
ppusher <thelastjediwarrior-cs50@yahoo.com>
2021-03-29 12:04:56 +0530

    Code cleanup

--- coding-bootcamp-testimonials-slider/script.js
+++ coding-bootcamp-testimonials-slider/script.js
@@ -1,10 +1,8 @@
-const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelectorAll(".prev");
+const nextBtn = document.querySelectorAll(".next");
 const slides = document.querySelectorAll(".slide");
 
 let index = 0;
-
-// active(index);
 
 function active(index) {
   slides.forEach(item => {
@@ -15,22 +13,20 @@ function active(index) {
 
 function nextSlide () {
   index++;
-  if(index > slides.length-1) {
+  if(index > slides.length-1){
     index = 0;
   }
-  console.log('clicked next')
   active(index);
 }
 
 function prevSlide () {
   index--;
-  console.log('clicked prev')
   if(index < 0) {
     index = slides.length-1;
   }
-  active(index)
-  
+  active(index);
 }
 
-nextBtn.addEventListener('click', nextSlide);
-prevBtn.addEventListener('click', prevSlide);
+active(index);
+nextBtn.forEach((item) => item.addEventListener("click", nextSlide));
+prevBtn.forEach((item) => item.addEventListener("click", prevSlide));
