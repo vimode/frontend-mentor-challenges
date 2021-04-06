7052629613a0b69603d817e616966821ea448457
ppusher <thelastjediwarrior-cs50@yahoo.com>
2021-04-06 12:56:58 +0530

    Updated form validation

--- base-apparel-coming-soon/script.js
+++ base-apparel-coming-soon/script.js
@@ -1,18 +1,29 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
+const toastmsg = document.querySelector('.toast-success');
 
-form.addEventListener('submit', e => {
-	e.preventDefault();
-  const emailValue = email.value;
+form.addEventListener('submit', validation) 
 
-  if(!validEmail(emailValue)) {
+email.addEventListener('input', activeValidation)
+
+function activeValidation (e) {
+  if (email.validity.patternMismatch) {
     form.classList.add('error');
   } else {
     form.classList.remove('error');
   }
-});
-
-function validEmail (email) {
-  let emailRegEx =  /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
-  return emailRegEx.test(String(email).toLowerCase());
 }
+
+function validation(e) {
+	e.preventDefault();
+
+  if (email.validity.valueMissing) {
+    form.classList.add('error') 
+  } else {
+      toastmsg.classList.add('show');
+    setTimeout(function() {
+      toastmsg.classList.remove('show');
+    },3000)
+  }
+
+};
