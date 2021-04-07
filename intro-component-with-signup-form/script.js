e21628711442aaef3fd23dfa4586da178b194829
ppusher <thelastjediwarrior-cs50@yahoo.com>
2021-04-07 11:13:32 +0530

    Updated form validation

--- intro-component-with-signup-form/script.js
+++ intro-component-with-signup-form/script.js
@@ -1,53 +1,45 @@
 const form = document.getElementById('form');
+const input = document.querySelectorAll('input');
+const btn = document.querySelector('.btn');
 
 const firstName = document.getElementById('first_name');
 const lastName = document.getElementById('last_name');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 
-const button = document.getElementsByClassName('btn');
+const toastmsg = document.querySelector('.toast-success');
 
+
+
+email.addEventListener('input', emailValidation)
+
+// This function is just check for the regEx pattern for valid email, the regEx pattern is part of the form markup. It does actively as the user types so removes error as soon as it passes the regEx pattern.
+function emailValidation (e) {
+  if (email.validity.patternMismatch) {
+    email.parentElement.classList.add('error');
+  } else {
+    email.parentElement.classList.remove('error');
+  }
+}
 
 form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  
-  if( firstName.value === '') {
-    showError(firstName);
-  } else {
-    removeError(firstName);
-  }
+  e.preventDefault(); 
 
-  if (lastName.value === '') {
-    showError(lastName);
-  } else {
-    removeError(lastName) ;
-  }
-
-  if (password.value === '') {
-    showError(password);
-  } else {
-    removeError(password);
-  }
-
-  if(email.value === '' && validEmail(email.value)) {
-    showError(email);
-  } else {
-    removeError(email);
-  }
-
-})
-
-function validEmail (email) {
-  let emailRegEx =  /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
-  return emailRegEx.test(String(email).toLowerCase());
-}
-
-function showError (field) {
-  const formElement = field.parentNode;
-  formElement.classList.add('error');
-}
-
-function removeError(field) {
-  const formElement = field.parentNode;
-  formElement.classList.remove('error');
-}
+  input.forEach((item) => {
+      if(item.validity.valueMissing) {
+          item.parentElement.classList.add('error');
+        } else {
+          if (item.name === "email") {
+            if (!email.validity.patternMismatch) {
+              item.parentElement.classList.add('error');
+            }
+          }
+          item.parentElement.classList.remove('error');
+          toastmsg.classList.add('show');
+          setTimeout(function() {
+            toastmsg.classList.remove('show');
+          },3000)
+        }
+  })
+});
+    
