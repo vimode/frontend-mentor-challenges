88a93aa39c90b597de8fb1fbc15a1f257ec8eb52
ppusher <thelastjediwarrior-cs50@yahoo.com>
2021-04-07 11:11:54 +0530

    Updated form validation JS

--- base-apparel-coming-soon/script.js
+++ base-apparel-coming-soon/script.js
@@ -3,27 +3,33 @@ const email = document.getElementById('email');
 const toastmsg = document.querySelector('.toast-success');
 
 email.addEventListener('input', activeValidation)
-
 form.addEventListener('submit', validation) 
 
-function activeValidation (e) {
-  if (email.validity.patternMismatch) {
-    form.classList.add('error');
-  } else {
+// Actively checks the user input 
+function activeValidation(e) {
+  if (email.validity.valid) {
     form.classList.remove('error');
+  } else {
+    validationError();
   }
 }
 
+// Check when clicked submit
 function validation(e) {
 	e.preventDefault();
-
-  if (email.validity.valueMissing || email.validity.patternMismatch) {
-    form.classList.add('error') 
+  if(!email.validity.valid) {
+    validationError();
   } else {
-      toastmsg.classList.add('show');
-    setTimeout(function() {
+    toastmsg.classList.add('show');
+      setTimeout(function() {
       toastmsg.classList.remove('show');
-    },3000)
+    },3000)   
   }
-
 };
+
+// Throws an e rror if doesn't match the regEx pattern or if its just an empty field
+function validationError() {
+  if(email.validity.patternMismatch || email.validity.valueMissing) {
+    form.classList.add('error')
+  } 
+}
