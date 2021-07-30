faaf2eda6fb1db8ad4af667963d717ccd30e68ae
ppusher <thelastjediwarrior-cs50@yahoo.com>
2021-07-30 12:36:03 +0530

    Added image fixes and some JS fix

--- blogr-landing-page/script.js
+++ blogr-landing-page/script.js
@@ -1,6 +1,7 @@
 const nav = document.querySelector('.navbar');
 const navToggle = document.querySelector('.nav_toggle');
 const navItem = document.querySelectorAll('.primary_nav_items');
+const primaryNav = document.querySelector('.primary_nav');
 
 
 
@@ -10,14 +11,23 @@ navToggle.addEventListener('click', () => {
 
 navItem.forEach((item, index) => {
   item.addEventListener('click', () => {
-    removeClass()
     item.parentNode.classList.toggle('openItem')
   })
 })
 
-let removeClass = function () {
-  for (item of navItem) {
-    item.parentNode.classList.remove('openItem')
-  }
-}
+// let navSelection = "";
+// primaryNav.addEventListener('click', (e) => {
+//   let selectedOption = e.target;
 
+//   if (navSelection) {
+//     navSelection.parentNode.classList.remove('openItem');
+//   }
+//   navSelection = selectedOption
+//   if (selectedOption.parentNode.classList.contains == 'openItem') {
+//     console.log("contains")
+//     selectedOption.parentNode.classList.remove('openItem');
+//   } else {
+//     console.log("does not")
+//     selectedOption.parentNode.classList.add('openItem');
+//   }
+// })
