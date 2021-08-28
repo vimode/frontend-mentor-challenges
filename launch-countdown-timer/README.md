4aed60507802d7ff2e733d4d8c1cc73b4a29fc1e
ppusher <thelastjediwarrior-cs50@yahoo.com>
2021-08-28 15:07:40 +0530

    Updated javascript

--- launch-countdown-timer/README.md
+++ launch-countdown-timer/README.md
@@ -29,12 +29,20 @@ Your users should be able to:
 ## Experience, Challenges and Learning
 
 _This will be updated periodically while the pending tasks are being worked on._
-https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/animationend_event
+
+This was trickier than I expected it to be, for the animation to work on any change in the countdown number my solution was to add two indetical cards on top of each other.
+The countdown card and an indentical card have the same properities and show the same number via the countdown js the only difference being that the flip box animates on change. T
+Each card is then further divived into two halves, top and the bottom part, on change (with the help of CSS animation) the top and the bottom part flips.
+With the help of `rotate3d()` the top card flips from top to the middle and the bottom card flips from middle to the end of the card.
+The animation still doesn't look perfect tand looks a little sticky and needs some work.
+
+The javascript too was fun, the logic is simple. Start the countdown, watch for change in the number and only then animate. For the animation part, it requires to add the animation and remove it otherwise it doesn't work continuously. So there is a callback function which is fired when `"animationend"` event happens.
+I really enjoyed learning about `animationend` at [MDN](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/animationend_event) had never heard of it or used it anyhwere. This opens a new possiblities of various quick CSS animations I could add via JavaScript.
 
 ---
 
 ##### Pending tasks
 
-- Only animate when theres change in the numbers.
+- The numbers still look very stick in the animation.
 - Live solution screenshot(s)
 - On submit update README with : Solution submitted at Frontend Mentor - [Submission Link]()
--- launch-countdown-timer/script.js
+++ launch-countdown-timer/script.js
@@ -1,4 +1,3 @@
-
 // Countdown --------------------------------- 00:00:00:00 //
 
 const days_view = document.querySelectorAll('.number_days');
@@ -6,7 +5,7 @@ const hours_view = document.querySelectorAll('.number_hours');
 const minutes_view = document.querySelectorAll('.number_minutes');
 const seconds_view = document.querySelectorAll('.number_seconds');
 
-// Todays daye
+// Today's date
 let today = new Date();
 
 // Today + 14/15 days
@@ -17,7 +16,6 @@ let showcaseDate = new Date(
 );
 
 // Calculate time for the countdown to display
-
 function showcaseCountdown() {
   let endTime = showcaseDate.getTime() - new Date().getTime();
 
@@ -26,12 +24,7 @@ function showcaseCountdown() {
   let minutes = Math.floor((endTime / 1000 / 60) % 60)
   let seconds = Math.floor((endTime / 1000) % 60)
 
-  return {
-    days: days,
-    hours: hours,
-    minutes: minutes,
-    seconds: seconds
-  }
+  return { days, hours, minutes, seconds }
 };
 
 // DOM changes
@@ -40,77 +33,59 @@ let timeInterval = setInterval(function () { }, 1000);
 function updateCountdown() {
   let time = showcaseCountdown();
 
-  if (time.seconds >= 0) {
-
+  if (+time.seconds >= 0) {
     seconds_view.forEach(view => {
-      view.innerHTML = ('0' + time.seconds).slice(-2)
+      view.textContent = ('0' + time.seconds).slice(-2)
     })
-
-    // let flipTopCardS = document.querySelector('.seconds .flip_card .flip_card_top');
-    // let flipBottomCardS = document.querySelector('.seconds .flip_card .flip_card_bottom');
-    // animateCard(flipTopCardS, flipBottomCardS);
+    let flipTopCardS = document.querySelector('.seconds .flip_card .flip_card_top');
+    let flipBottomCardS = document.querySelector('.seconds .flip_card .flip_card_bottom');
+    animateCard(flipTopCardS, flipBottomCardS);
   }
 
-  if (minutes_view[0].textContent !== time.minutes && time.minutes >= 0) {
+  if (+minutes_view[0].textContent !== time.minutes && time.minutes >= 0) {
     minutes_view.forEach(view => {
-      view.innerHTML = ('0' + time.minutes).slice(-2)
+      view.textContent = ('0' + time.minutes).slice(-2)
     })
+    let flipTopCardM = document.querySelector('.minutes .flip_card .flip_card_top');
+    let flipBottomCardM = document.querySelector('.minutes .flip_card .flip_card_bottom');
+    animateCard(flipTopCardM, flipBottomCardM);
   }
 
-  if (hours_view[0].textContent !== time.hours && time.hours >= 0) {
+  if (+hours_view[0].textContent !== time.hours && time.hours >= 0) {
     hours_view.forEach(view => {
-      view.innerHTML = ('0' + time.hours).slice(-2)
+      view.textContent = ('0' + time.hours).slice(-2)
     })
+    let flipTopCardH = document.querySelector('.hours .flip_card .flip_card_top');
+    let flipBottomCardH = document.querySelector('.hours .flip_card .flip_card_bottom');
+    animateCard(flipTopCardH, flipBottomCardH);
   }
 
-  if (days_view[0].textContent !== time.days && time.days >= 0) {
+  if (+days_view[0].textContent !== time.days && time.days >= 0) {
     days_view.forEach(view => {
-      view.innerHTML = ('0' + time.days).slice(-2)
+      view.textContent = ('0' + time.days).slice(-2)
     })
+    let flipTopCardD = document.querySelector('.days .flip_card .flip_card_top');
+    let flipBottomCardD = document.querySelector('.days .flip_card .flip_card_bottom');
+    animateCard(flipTopCardD, flipBottomCardD);
   }
 
 }
+
+// Animate the card
+function animateCard(topCard, bottomCard) {
+  topCard.style.animation = "0.5s linear flipDown";
+  bottomCard.style.animation = "0.5s linear flipLower"
+  let removeAnimation = function () {
+    topCard.style.animation = "";
+    topCard.removeEventListener("animationed", removeAnimation);
+    bottomCard.style.animation = "";
+    bottomCard.removeEventListener("animationed", removeAnimation);
+  }
+  topCard.addEventListener("animationend", removeAnimation);
+  bottomCard.addEventListener("animationend", removeAnimation)
+}
+
 
 timeInterval = setInterval(function () {
   updateCountdown();
 }, 1000);
-
-
-
-// Animate the card
-function animateCard(topCard, bottomCard, time) {
-  topCard.style.animation = "1s linear flipDown";
-  bottomCard.style.animation = "1st linear flipLower"
-
-  // let removeAnimationTop = function () {
-
-  // }
-
-  // remove animation or add animation once animation is done
-  // topCard.addEventListener("animationed", removeAnimationTop);
-  // bottomCard.addEventListener("animationed", removeAnimationBottom)
-}
-
-
-// First attempt method to update without animating via JS
-
-// let activeCountdown = setInterval(function () {
-//   let today = new Date();
-
-//   let diff = showcaseDate.getTime() - today.getTime();
-
-//   updateTimer(days_view, Math.floor(diff / (1000 * 60 * 60 * 24)));
-//   updateTimer(hours_view, Math.floor((diff / (1000 * 60 * 60)) % 24));
-//   updateTimer(minutes_view, Math.floor((diff / 1000 / 60) % 60));
-//   updateTimer(seconds_view, Math.floor((diff / 1000) % 60));
-//   ;
-// }, 1000)
-
-
-
-// function updateTimer(fragment, calc) {
-//   fragment.forEach(item => {
-//     item.textContent = calc;
-//   })
-// }
-
