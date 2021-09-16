c2522bec97cb74404e7f3c5e569382ad64442472
ppusher <thelastjediwarrior-cs50@yahoo.com>
2021-09-17 01:22:07 +0530

    Added github-user-search-app  challenge

---
+++ github-user-search-app/README.md
@@ -0,0 +1,54 @@
+# GitHub user search app
+
+Solution : [Live Site URL](https://frontend-mentor-challenges-ecru.vercel.app/github-user-search-app/)
+
+This is a solution to the [GitHub user search app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/github-user-search-app-Q09YOgaH6).
+
+![Design preview for the GitHub user search app coding challenge](./preview.jpg)
+
+## The challenge
+
+Your challenge is to build out this GitHub user search app using the [GitHub users API](https://docs.github.com/en/rest/reference/users#get-a-user) and get it looking as close to the design as possible.
+
+You can use any tools you like to help you complete the challenge. So if you've got something you'd like to practice, feel free to give it a go.
+
+Your users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Search for GitHub users by their username
+- See relevant user information based on their search
+- Switch between light and dark themes
+- **Bonus**: Have the correct color scheme chosen for them based on their computer preferences. _Hint_: Research `prefers-color-scheme` in CSS.
+
+## Built with
+
+- HTML
+- CSS
+- CSS Grid
+- CSS Flexbox
+- JavaScript
+- Github API
+
+## Experience, Challenges and Learning
+
+_This will be updated periodically while the pending tasks are being worked on._
+
+https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/sidebarAction/toggle
+
+https://developer.mozilla.org/en-US/docs/Web/API/Window/matchMedia
+
+https://web.dev/prefers-color-scheme
+
+---
+
+##### Pending tasks
+
+- Mobile view footer element padding
+- 950px whitespace between hero and next element
+- slider title alignment fix
+- Add animations to the mobile nav and horizontal slider.
+- Fix wave pattern positioning.
+- Fix mobile nav positioning.
+- Live solution screenshot(s)
+- On submit update README with : Solution submitted at Frontend Mentor - [Submission Link]()
---
+++ github-user-search-app/script.js
@@ -0,0 +1,122 @@
+
+// fetch changes
+
+
+const searchForm = document.querySelector('.search_bar')
+const searchInput = document.querySelector('#dev_search')
+const btn = document.querySelectorAll('.btn');
+
+// btn.forEach(item => item.addEventListener('click', (e) => e.preventDefault()))
+
+// searchForm.addEventListener('submit', () => {
+//   console.log(searchForm.value)
+// })
+
+const formData = new FormData();
+const errorText = document.querySelector('.error_text');
+
+// profile data
+const pCard = document.querySelector(".profile_card")
+const pImgSrc = document.querySelector(".profile_img_src");
+const pName = document.querySelector(".profile_name");
+const pUsername = document.querySelector(".profile_username");
+const pDate = document.querySelector(".profile_date");
+const pBio = document.querySelector(".profile_bio");
+const pRepo = document.querySelector(".repo_number");
+const pFollowers = document.querySelector(".followers_number");
+const pFollowing = document.querySelector(".following_number");
+const pLocation = document.querySelector(".info_location_text");
+const pURL = document.querySelector(".info_website_text");
+const pTwitter = document.querySelector(".info_twitter_text");
+const pCompany = document.querySelector(".info_company_text");
+
+
+// fetch data
+async function searchUser(event) {
+  event.preventDefault()
+
+  let queryString = searchInput.value;
+  let endPointUrl = `https://api.github.com/users/${queryString}`
+
+  const response = await fetch(endPointUrl);
+
+  if (response.ok) {
+    const searchResult = await response.json();
+
+    let { avatar_url, html_url, name, login, bio, public_repos, followers, following, location, blog, twitter_username, company, created_at } = searchResult;
+
+    let createdDate = new Date(created_at).toLocaleString('en-GB', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric',
+    });
+
+    pImgSrc.src = avatar_url;
+    name ? pName.textContent = name : pName.textContent = login
+    pUsername.href = html_url;
+    pUsername.textContent = `@${login}`;
+    pDate.textContent = `Joined ${createdDate}`;
+    bio ? pBio.textContent = bio : pBio.textContent = `Lorem ipsum dolor sit, amet consectetur adipisicing elit. Obcaecati, eum? Eveniet, ullam
+    vel laborum corrupt.`;
+    pRepo.textContent = public_repos;
+    pFollowers.textContent = followers;
+    pFollowing.textContent = following;
+    location ? pLocation.textContent = location : pLocation.textContent = `Not Available`;
+    blog ? pURL.href = `https://${blog}` : pURL.href = "https:/github.com";
+    blog ? pURL.textContent = blog : pURL.textContent = `Not Available`;
+    twitter_username ? pTwitter.textContent = twitter_username : pTwitter.textContent = 'Not Available';
+    company ? pCompany.textContent = company : pCompany.textContent = "Not Available";
+
+    pCard.classList.add('show');
+
+    searchInput.value = "";
+  } else {
+    errorText.classList.add('show');
+    setTimeout(function () {
+      errorText.classList.remove('show')
+    }, 2000);
+  }
+}
+
+// theme toggle
+
+const labelText = document.querySelector('#mode');
+const body = document.querySelector('body')
+// dark theme /media prefers-color-scheme 
+const darkSide = window.matchMedia('(prefers-color-scheme: dark)');
+
+const themeSwitcherToggle = document.querySelector('.toggle_theme');
+
+themeSwitcherToggle.addEventListener('change', () => {
+  darkSideStatus(themeSwitcherToggle.checked)
+})
+
+
+// switch media queries
+
+function darkSideStatus(status) {
+  body.classList.toggle('dark_theme', status);
+  console.log(status)
+  if (status) {
+    labelText.textContent = 'LIGHT';
+    // themeSwitcherToggle.checked = "true"
+  } else {
+    labelText.textContent = 'DARK';
+    // themeSwitcherToggle.checked = "false"
+  }
+}
+
+// todo : on browser pref change, wrong toggle icon/text is displayed.
+
+// run to check the preferred browser theme 
+// the matchMedia has  a property of matches which returns true/false
+darkSideStatus(darkSide.matches)
+
+// listen to changes to switch theme on fly
+darkSide.addListener((e) => {
+  darkSideStatus(e.matches)
+})
+
+
+
+
