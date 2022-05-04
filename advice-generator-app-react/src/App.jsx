62d741d0a170aaf6f7fe19a02e291a7986ef8b8e
ppusher <thelastjediwarrior-cs50@yahoo.com>
2022-05-04 17:06:38 +0530

    Updated components and styles

--- advice-generator-app-react/src/App.jsx
+++ advice-generator-app-react/src/App.jsx
@@ -1,10 +1,48 @@
+import { useState, useEffect } from 'react';
 
-import './App.css'
+import { QuoteWrapper, QuoteTitle, QuoteText,
+  Button, QuoteImage, CircleLoader } from "./App.styles";
 
 function App() {
+
+const [newQuote, setNewQuote] = useState();
+const [isLoading, setisLoading] = useState(false);
+
+  function fetchAdvice() {
+    setisLoading(true)
+    setNewQuote()
+    const uri = "https://api.adviceslip.com/advice";    
+   fetch(uri)
+      .then((r) => { if (r.ok)  return r.json()
+      throw new Error(`Network response ${r.status}`)
+      })
+      .then((data)=>setNewQuote(data));
+      setisLoading(false)
+  }
+
+  useEffect(() => {
+    fetchAdvice()
+  }, [])
   
   return (
-    <h1>Advice Generator App</h1>
+
+    <QuoteWrapper>
+    {newQuote && !isLoading ? 
+      <>
+        <QuoteTitle>
+          Advice #{newQuote.slip.id}
+        </QuoteTitle>
+        <QuoteText>
+          "{newQuote.slip.advice}"
+        </QuoteText>
+        <QuoteImage src="./images/pattern-divider-desktop.svg"/>
+        </>
+        :  
+      <CircleLoader /> }
+
+  <Button onClick = {()=>fetchAdvice()}><img src="./images/icon-dice.svg"></img></Button>
+    </QuoteWrapper>
+
   )
 }
 
---
+++ advice-generator-app-react/src/GlobalStyles.js
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  scroll-behavior:  smooth;
+}
+
+:root {
+  --ff-primary : 'Manrope', sans-serif;
+
+  --fw-exbold: 800;
+
+  --fs-quote: 28px;
+
+  --clr-primary-text: hsl(193, 38%, 86%);
+  --clr-primary-green: hsl(150, 100%, 66%);
+  --clr-neutral-bg-blue: hsl(218, 23%, 16%);
+  --clr-neutral-gray-blue: hsl(217, 19%, 38%);
+  --clr-neutral-dark-blue: hsl(217, 19%, 24%);
+}
+
+body {
+  font-family: var(--ff-primary);
+  color:var(--clr-primary-text);
+  background-color: var(--clr-neutral-bg-blue);
+  height: 100vh;
+  font-size: var(--fs-quote);
+}
+
+#root {
+  display: flex;
+  height: 100%;
+  place-items: center;
+  place-content: center;
+}
+
+@media(max-width: 768px) {
+  :root {
+    --fs-quote: 18px;
+  }
+}
+`
