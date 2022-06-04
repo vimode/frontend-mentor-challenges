ba875cdf07a65a8e8dae6dd43a822d98b2bf3315
ppusher <thelastjediwarrior-cs50@yahoo.com>
2022-06-04 20:28:53 +0530

    Added custom icon and mapCenter

--- ip-address-tracker/src/components/MapData.jsx
+++ ip-address-tracker/src/components/MapData.jsx
@@ -1,12 +1,5 @@
-import { useEffect } from "react";
-
-import styled from "styled-components";
-
-import { MapWrapper } from "./MapData.styles";
-
-import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
-
-
+import { MapContainer, TileLayer, useMap, Marker } from 'react-leaflet';
+import { Icon } from "leaflet";
 
 function MapData ({IPinfo}) {
 
@@ -14,28 +7,37 @@ function MapData ({IPinfo}) {
   const location = [51.505, -0.09]
   const zoomLevel = 13;
 
+  let mapCenter = location
+
   if (IPinfo) {
     location[0] = IPinfo.lat;
     location[1] = IPinfo.long
+    mapCenter = [location[0], location[1] + 100]
   } 
 
-
-  //flyTo Map method tot update the map view when co-ordinates change
-  //https://leafletjs.com/reference.html#map-flyto
+ //flyTo Map method to update the map view when co-ordinates change, this hook returns component
+ //https://leafletjs.com/reference.html#map-flyto
   function FlyToNewLocation() {
     const map = useMap()
     map.flyTo(location, zoomLevel, {duration: 3})
     return null
   }
 
+  // custom marker icon
+  const blackMarker = new Icon({
+    iconUrl : "./images/icon-location.svg",
+    iconSize :  [30,38]
+  });
+
+
 
   return (
-      <MapContainer center={location} zoom={zoomLevel} scrollWheelZoom={false}>
+      <MapContainer center={mapCenter} zoom={zoomLevel} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={location}>
+        <Marker position={location} icon ={blackMarker}>
         </Marker>
         <FlyToNewLocation />
       </MapContainer> 
