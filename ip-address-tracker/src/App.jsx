6a4100cc1b87269c5cbda7db8bb0c30653a89168
ppusher <thelastjediwarrior-cs50@yahoo.com>
2022-06-04 20:25:34 +0530

    Cleaned up comments and API URIs

--- ip-address-tracker/src/App.jsx
+++ ip-address-tracker/src/App.jsx
@@ -2,26 +2,9 @@ import { useState, useEffect } from "react";
 
 import GlobalStyle from "./GlobalStyle";
 
-import { ThemeProvider } from "styled-components";
-import { lightTheme,darkTheme } from "./themes";
 import { OuterWrapper } from "./App.styles.js";
 
 import { Header, IPData, MapData } from "./components";
-
-  // make App the layout componet itself which has the top page title, bg image,  input component, data component, map component
-  // state to save fetched data from the IP
-  // bonus : theme switcher
-
-  // create input component - function prop to pass ip info
-      // state to hold a demo IP and/or userInput 
-      // useRef to force focus in input
-  
-  // create data component to load fetched data
-    // fill data with  props from parent
-  
-  // create a map component to load leaftlet map with the marker
-    // function to process fetched data and pass to the leaflet map function to load map and the pin
-
 
 
 function App() {
@@ -30,10 +13,9 @@ function App() {
   const [IPinfo, setIPinfo] = useState();
 
   async function fetchUserIP () {
-    const URI = "https://api64.ipify.org/"
-    const TESTURI = 'https://api.allorigins.win/raw?url=https://api64.ipify.org/'
+    const URI = "https://api.my-ip.io/ip"
 
-    const response = await fetch(TESTURI);
+    const response = await fetch(URI);
     const userIPAddress = await response.text()
     setUserIP(userIPAddress)
   }
@@ -41,26 +23,28 @@ function App() {
   async function fetchLocationInfo(IP) {
     try {
       const URI = `http://ip-api.com/json/${IP}`
+      const CORSURI = `https://api.allorigins.win/raw?url=http://ip-api.com/json/${IP}`
       const URI_2 = `https://freeipapi.com/api/json/${IP}`
-      const TESTURI = `https://api.allorigins.win/raw?url=http://ip-api.com/json/${IP}`
-      const TESTURI2 = `https://api.allorigins.win/raw?url=https://freeipapi.com/api/json/${IP}`
 
       const fetchThemAll = await Promise.all([
-        fetch(TESTURI).then((res) => res.json()),
-        fetch(TESTURI2).then((res) =>res.json())
-      ].map(promise => promise.catch(error => error)));
+        fetch(CORSURI),
+        fetch(URI_2)
+      ]);
+      const allData = await Promise.all(fetchThemAll.map( r=> r.json()))
 
-      console.log(fetchThemAll)
-
-      let combinedFetchedData = {
-        ip : fetchThemAll[0].query,
-        location : fetchThemAll[0].country,
-        tz : fetchThemAll[1].timeZone,
-        isp: fetchThemAll[0].isp,
-        lat: fetchThemAll[0].lat,
-        long: fetchThemAll[0].lon,
+      if (allData[0].status === "success") {
+        let combinedFetchedData = {
+          ip : allData[1].ipAddress,
+          location : allData[0].country,
+          tz : allData[1].timeZone,
+          isp: allData[0].isp,
+          lat: allData[0].lat,
+          long: allData[0].lon,
+        }        
+        setIPinfo(combinedFetchedData);
+      } else {
+        setUserIP("8.8.8.8")
       }
-      setIPinfo(combinedFetchedData);
     } catch (error) {
       console.log(error)
     }
@@ -78,10 +62,9 @@ function App() {
     if(UserIP) fetchLocationInfo(UserIP)
   }, [UserIP])  
 
-  
 
   return (
-      <ThemeProvider theme= {lightTheme}>
+      <>
         <GlobalStyle />
         <OuterWrapper>
           <Header 
@@ -94,7 +77,7 @@ function App() {
             IPinfo = {IPinfo}
           />
         </OuterWrapper>
-      </ThemeProvider>
+      </>
   )
 }
 
