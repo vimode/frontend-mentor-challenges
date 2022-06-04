5e269ba70fc67d201cf2c3eb40be75a87a480399
ppusher <thelastjediwarrior-cs50@yahoo.com>
2022-06-04 20:28:04 +0530

    Added media queries

--- ip-address-tracker/src/components/IPData.styles.js
+++ ip-address-tracker/src/components/IPData.styles.js
@@ -1,48 +1,66 @@
 import styled from "styled-components";
 
-import { FlexWrapper } from "./../Utility.style";
-
 export const DataWrapper = styled.section`
     position: absolute;
     z-index: 500;
-    top: 40%;
+    top: 350px;
     left: 50%;
     transform: translate(-50%, -50%);
+
+    @media(min-width:  768px ) { 
+      top: 300px;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
   `
   
   export const DataList = styled.ul`
-  display:flex;
-  align-items:start;
-  width: 100%;
-  background: var(--color-white);
-  border-radius: 10px;
-  padding-block: 50px;
+    display:flex;
+    flex-direction: column;
+    width: 80vw;
+    background: var(--color-white);
+    border-radius: 10px;
+    padding-block: 30px;
+    align-items: center;
+    gap: 20px;
+    
+  @media(min-width: 768px) {
+    flex-direction:row;
+    width:100%;
+    align-items:start;
+    gap: unset;
+  }
 `
 
 export const ListItem = styled.li`
   list-style: none;
   display: flex;
   flex-direction: column;
-  padding-inline: 50px;
   position: relative;
   height: max-content;
   gap: 10px;
+  place-items:center;
+
+
+  @media (min-width:  768px) {
+    place-items: start;
+    padding-inline: 30px;
+
+    &:after {
+      content : "";
+      height: 70px;
+      width: 1px;
+      background: var(--color-secondary-gray);
+      position: absolute;
+      right: 0;
+      top:0;
+    }
+
+    &:last-child:after {
+      display:none;
+    }
+  }
   
-
-  &:after {
-    content : "";
-    height: 70px;
-    width: 1px;
-    background: var(--color-secondary-gray);
-    position: absolute;
-    right: 0;
-    top:0;
-    
-  }
-
-  &:last-child:after {
-    display:none;
-  }
 `
 
 export const Title = styled.h2`
@@ -50,11 +68,18 @@ export const Title = styled.h2`
   text-transform: uppercase;
   letter-spacing: 2px;
   color: var(--color-secondary-gray);
+  width:max-content;
 `
 
 export const Content = styled.p`
   font-size: 1.5rem;
   font-weight: var(--fw-med);
-  width:max-content;
-  max-width: 10ch;
+  width: max-content;
+  text-align:center;
+  max-width: ${props => props.ispinfo ? "13ch" : "max-content"};
+
+  @media (min-width: 768px) {
+    text-align: left;
+  }
+
 `
