cb60cb864f2b3623236096c46be609f750fabb53
vimo <vidyesh@yahoo.com>
2022-07-09 21:06:20 +0530

    [feat]: Add responsive image sets

--- minimalist-portfolio-website/src/components/Summary.jsx
+++ minimalist-portfolio-website/src/components/Summary.jsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom";
 import { SummaryWrapper, ContentWrapper } from "./Summary.styles"
 import { PrimaryStyledLink } from "./LinkButton.styles";
 
-function Summary ({title, content, link, linktext, imgsrc}) {
+function Summary ({title, content, link, linktext, imgsrc, order}) {
+
   return (
     <SummaryWrapper>
-      <picture>
-        <source srcset={imgsrc[0]} media = "(min-width: 1500px)"/>
-        <source srcset={imgsrc[1]} media="(min-width: 400px)" />
+      <picture className={order}>
+        <source srcSet={imgsrc[0]} media = "(min-width: 1920px)"/>
+        <source srcSet={imgsrc[1]} media="(min-width: 400px)" />
         <img src ={imgsrc[1]} alt={title} />
       </picture>
       <ContentWrapper>
