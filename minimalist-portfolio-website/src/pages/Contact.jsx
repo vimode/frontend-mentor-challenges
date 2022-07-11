a3315f93308243bde3ec7eb757a21faf1931c433
vimo <vidyesh@yahoo.com>
2022-07-11 23:43:10 +0530

    refactor(html entity): add html entity to contact page

--- minimalist-portfolio-website/src/pages/Contact.jsx
+++ minimalist-portfolio-website/src/pages/Contact.jsx
@@ -5,7 +5,7 @@ function Contact () {
     <main>
       <div>
         <h1>Get in Touch</h1>
-        <p>I’d love to hear about what you’re working on and how I could help. I’m currently looking for a new role and am open to a wide range of opportunities. My preference would be to find a position in a company in London. But I’m also happy to hear about opportunites that don’t fit that description. I’m a hard-working and positive person who will always approach each task with a sense of purpose and attention to detail. Please do feel free to check out my online profiles below and get in touch using the form.</p>
+        <p>I&apos;d love to hear about what you&apos;re working on and how I could help. I&apos;m currently looking for a new role and am open to a wide range of opportunities. My preference would be to find a position in a company in London. But I&apos;m also happy to hear about opportunites that don&apos;t fit that description. I&apos;m a hard-working and positive person who will always approach each task with a sense of purpose and attention to detail. Please do feel free to check out my online profiles below and get in touch using the form.</p>
         <SocialIcons />
       </div>
 
