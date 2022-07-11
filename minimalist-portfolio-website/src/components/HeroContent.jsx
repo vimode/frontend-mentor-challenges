861eb5900a52d13d2af94a085b7b8ddb660c2f02
vimo <vidyesh@yahoo.com>
2022-07-11 23:39:27 +0530

    refactor(herocontent): added html entity

--- minimalist-portfolio-website/src/components/HeroContent.jsx
+++ minimalist-portfolio-website/src/components/HeroContent.jsx
@@ -6,7 +6,7 @@ function HeroContent () {
     <HeroWrapper>
       <img src="/images/homepage/desktop/image-homepage-hero@2x.jpg" />
       <HeroContentWrapper>
-        <h1>Hey, I'm Alex Spencer and I love bulding beautiful websites</h1>
+        <h1>Hey, I&apos;m Alex Spencer and I love bulding beautiful websites</h1>
         <LinkButton link="/portfolio" linkText="About Me"></LinkButton>
       </HeroContentWrapper>
     </HeroWrapper>
