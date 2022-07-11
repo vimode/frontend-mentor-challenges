719a08425774fce7ba615c3d706e93d00c5d4cb7
vimo <vidyesh@yahoo.com>
2022-07-11 23:44:06 +0530

    fixed characater

--- minimalist-portfolio-website/src/data/data.js
+++ minimalist-portfolio-website/src/data/data.js
@@ -14,7 +14,7 @@ export const projectData = [
     intro: "This project required me to build a fully responsive landing page to the designs provided. I used HTML5, along with CSS Grid and JavaScript for the areas that required interactivity, such as the testimonial slider.",
     type: ["Interaction Design", "Front End Development"],
     tags: ["HTML", "CSS", "JS"],
-    background: `This project was a front-end  challenge from Frontend Mentor. It’s a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.`,
+    background: `This project was a front-end  challenge from Frontend Mentor. It's a platform that enables you to practice building websites to a design and project brief. Each challenge includes mobile and desktop designs to show how the website should look at different screen sizes. Creating these projects has helped me refine my workflow and solve real-world coding problems. I’ve learned something new with each project, helping me to improve and adapt my style.`,
     coverimg: "/images/detail/desktop/image-manage-hero@2x.jpg",
     previews: ["/images/detail/desktop/image-manage-preview-1@2x.jpg","/images/detail/desktop/image-manage-preview-2@2x.jpg"],
     portfolioImg: ["/images/portfolio/desktop/image-portfolio-manage.jpg","/images/portfolio/desktop/image-portfolio-manage@2x.jpg"],
