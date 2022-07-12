85bb26839036273ea097371b63e0647e4074b889
vimo <vidyesh@yahoo.com>
2022-07-12 11:45:41 +0530

    style(layout): fix layout and position center

--- minimalist-portfolio-website/src/App.styles.js
+++ minimalist-portfolio-website/src/App.styles.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 export const OuterWrapper = styled.section `
   display: flex;
   flex-direction: column;
+  place-content: center;
 `
 export const HeaderWrapper = styled.header`
 `
@@ -12,6 +13,9 @@ export const MainWrapper= styled.main`
   flex-direction: column;
   gap: var(--space-16);
   place-items:center;
+  max-width: 1200px;
+  margin: 0 auto;
 `
 export const FooterWrapper = styled.footer`
+    background: var(--color-primary-grey-blue-800);
 `
