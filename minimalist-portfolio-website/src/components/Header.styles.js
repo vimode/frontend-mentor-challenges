9f906b6d431cff74e82d3c9cbebf176e67af987f
vimo <vidyesh@yahoo.com>
2022-07-13 20:18:06 +0530

    style[spacing]: Update padding

--- minimalist-portfolio-website/src/components/Header.styles.js
+++ minimalist-portfolio-website/src/components/Header.styles.js
@@ -4,6 +4,7 @@ export const HeaderWrapper = styled.header `
   display: flex;
   place-content: space-between;
   padding-block: var(--space-8);
+  padding-bottom: var(--space-20);
   max-width: 1200px;
   width: 90%;
   margin: 0 auto;
