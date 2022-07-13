f38ebe6bc716aa21c886d2133e5681d3ad0206e8
vimo <vidyesh@yahoo.com>
2022-07-13 20:25:24 +0530

    style: styled components initiated

---
+++ minimalist-portfolio-website/src/pages/Contact.styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const AboutWrapper   = styled.div`
+
+`
+
+export const ContactWrapper = styled(AboutWrapper)`
+
+`
+
+export const ContactForm = styled.form`
+
+`
