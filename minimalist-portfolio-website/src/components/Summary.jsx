d5942ca166aa02f1df0e4d53aa4180ef62983e23
vimo <vidyesh@yahoo.com>
2022-07-13 20:23:17 +0530

    feat[prop]: add option to accept homepage related prop

--- minimalist-portfolio-website/src/components/Summary.jsx
+++ minimalist-portfolio-website/src/components/Summary.jsx
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 import { SummaryWrapper, ContentWrapper } from "./Summary.styles"
 import { PrimaryStyledLink } from "./LinkButton.styles";
 
-function Summary ({title, content, link, linktext, imgsrc, order}) {
+function Summary ({title, content, link, linktext, imgsrc, order,homepage}) {
 
   return (
-    <SummaryWrapper>
+    <SummaryWrapper homepage={homepage}>
       <picture className={order}>
-        <source srcSet={imgsrc[0]} media = "(min-width: 1920px)"/>
+        <source srcSet={imgsrc[0]} media = "(min-width: 900px)"/>
         <source srcSet={imgsrc[1]} media="(min-width: 400px)" />
         <img src ={imgsrc[1]} alt={title} className="summary" />
       </picture>
-      <ContentWrapper>
+      <ContentWrapper homepage={homepage}>
         <h2>{title}</h2>
         <p>{content}</p>
         {link && <PrimaryStyledLink to={link}>{linktext}</PrimaryStyledLink>}
