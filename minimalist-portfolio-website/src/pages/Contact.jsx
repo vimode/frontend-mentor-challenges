8353f6ba36cc52b0e812760b6097e3d46687d4fa
vimo <vidyesh@yahoo.com>
2022-07-13 20:24:19 +0530

    feat[components]: Add styled components

--- minimalist-portfolio-website/src/pages/Contact.jsx
+++ minimalist-portfolio-website/src/pages/Contact.jsx
@@ -1,17 +1,18 @@
 import { SocialIcons } from "../components";
+import { AboutWrapper, ContactWrapper, ContactForm } from "./Contact.styles";
 
 function Contact () {
   return (
-    <main>
-      <div>
+    <>
+      <AboutWrapper>
         <h1>Get in Touch</h1>
         <p>I&apos;d love to hear about what you&apos;re working on and how I could help. I&apos;m currently looking for a new role and am open to a wide range of opportunities. My preference would be to find a position in a company in London. But I&apos;m also happy to hear about opportunites that don&apos;t fit that description. I&apos;m a hard-working and positive person who will always approach each task with a sense of purpose and attention to detail. Please do feel free to check out my online profiles below and get in touch using the form.</p>
         <SocialIcons />
-      </div>
+      </AboutWrapper>
 
-      <div>
+      <ContactWrapper>
         <h1>Contact Me</h1>
-        <form>
+        <ContactForm>
           <label>Name</label>
           <input type="text"/>
           <label>Email Address</label>
@@ -20,11 +21,11 @@ function Contact () {
           <textarea />
           <button>Send Message</button>
 
-        </form>
-      </div>
+        </ContactForm>
+      </ContactWrapper>
       
-    </main>
+    </>
   )
 }
 
-export default  Contact;
+export default Contact;
