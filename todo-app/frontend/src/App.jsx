e7da9902a8986bacba7be6f3d69e3a085584ae6f
vimo <vidyesh@yahoo.com>
2022-12-08 22:24:07 +0530

    Update: moved components

--- todo-app/frontend/src/App.jsx
+++ todo-app/frontend/src/App.jsx
@@ -1,35 +1,86 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+
 import TodoWrapper from "./components/Todowrapper.jsx";
-import "./App.scss"
+import Header from "./components/Header.jsx";
+import "./App.scss";
 
-// Header component
-function Header() {
-  return (
-    <header>
-      <h1>TODO</h1>
-      {/* maybe a radio input
-      <button>theme toggle</button> */}
-    </header>
-  );
-}
+import registrationService from "./services/users.js";
+import { clearLocalState } from "./reducers/todoReducer.jsx";
+import { loginUser, authUser, logoutUser } from "./reducers/userReducer.jsx";
 
-// Footer component
-function Footer () {
-  return (
-  <footer>
-    Created by <a href="#">vimo</a>
-  </footer>
-  )
-}
-
-// App Component
 function App() {
+  const dispatch = useDispatch();
+  const loggedUser = useSelector((state) => state.user);
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem("loggedTodoUser");
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON);
+      dispatch(authUser(user));
+    }
+  }, []);
+
+  async function handleLogin(event) {
+    event.preventDefault();
+
+    try {
+      dispatch(loginUser(username, password));
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      console.error("wrong credentials");
+    }
+  }
+
+  async function handleRegistration(event) {
+    event.preventDefault();
+    const userDetails = { name, username, password };
+    try {
+      await registrationService.register(userDetails);
+      setName("")
+      setUsername("");
+      setPassword("");
+    } catch (exception) {
+      console.error(exception);
+    }
+  }
+
+  function handleLogout() {
+    dispatch(logoutUser());
+    dispatch(clearLocalState([]));
+  }
+
   return (
     <div className="App">
-      <Header />
+      <Header
+        username={username}
+        password={password}
+        setUsername={setUsername}
+        setPassword={setPassword}
+        name={name}
+        setName={setName}
+        handleLogin={handleLogin}
+        handleRegistration={handleRegistration}
+        handleLogout={handleLogout}
+      />
       <TodoWrapper />
-      <Footer />
+      {/* <Footer /> */}
     </div>
   );
 }
 
 export default App;
+
+// Footer component
+function Footer() {
+  return (
+    <footer>
+      Created by <a href="#">vimo</a>
+    </footer>
+  );
+}
