1831590c3d437e173cc2b02a7067cb2275c7f67c
vimo <vidyesh@yahoo.com>
2022-12-13 20:18:32 +0530

    Update: res to send userData

--- todo-app/backend/controllers/login.js
+++ todo-app/backend/controllers/login.js
@@ -1,27 +1,41 @@
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
-const loginRouter = require('express').Router()
-const User = require('../models/user')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const loginRouter = require("express").Router();
+const User = require("../models/user");
 
-loginRouter.post('/', async (request, response) => {
-  const { username, password } = request.body
+loginRouter.post("/", async (request, response) => {
+  const { username, password } = request.body;
 
-  const user = await User.findOne({ username })
+  const user = await User.findOne({ username });
 
-  const validPassword = user === null ? false : await bcrypt.compare(password, user.passwordHash)
+  const validPassword =
+    user === null ? false : await bcrypt.compare(password, user.passwordHash);
 
   if (!(user && validPassword)) {
-    return response.status(401).json({ error: 'Invalid username or password' })
+    return response.status(401).json({ error: "Invalid username or password" });
   }
 
   const userForToken = {
     username: user.username,
     id: user._id,
-  }
+  };
 
-  const token = jwt.sign(userForToken, process.env.JWT_SECRET, { expiresIn: 60*1440 })
+  const token = jwt.sign(userForToken, process.env.JWT_SECRET, {
+    expiresIn: 60 * 1440,
+  });
 
-  response.status(200).send({ token, username: user.username, name: user.name, })
-})
+  const userData = await User.findOne({ username }, "name username").populate(
+    "todos"
+  );
 
-module.exports = loginRouter
+  response
+    .status(200)
+    .send({
+      token,
+      username: userData.username,
+      name: userData.name,
+      todos: userData.todos,
+    });
+});
+
+module.exports = loginRouter;
