d55a1691c563cf41f4dc775b0a408404bd891937
vimo <vidyesh@yahoo.com>
2022-12-20 20:51:49 +0530

    add new api endpoint

--- todo-app/frontend/src/services/login.js
+++ todo-app/frontend/src/services/login.js
@@ -1,9 +1,18 @@
 import axios from "axios";
-const baseURL = "http://localhost:6003/api/login"
+const baseURL = "http://localhost:6003/api/login";
+const baseUserURL = "http://localhost:6003/api/users"
 
 const login = async (credentials) => {
   const response = await axios.post(baseURL, credentials);
   return response.data;
 };
 
-export default { login };
+const loggedInData = async (user) => {
+  const config = {
+    headers: { Authorization: `Bearer ${user.token}`},
+  };
+  const response = await axios.get(`${baseUserURL}/${user.id}`, config);
+  return response.data;
+};
+
+export default { login, loggedInData };
