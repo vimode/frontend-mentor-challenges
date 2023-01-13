cd2770619575f24cf0865af5bcfb59df8acbd32d
vimo <vidyesh@yahoo.com>
2023-01-13 18:21:20 +0530

    update vite config to change deploy

--- todo-app/frontend/vite.config.js
+++ todo-app/frontend/vite.config.js
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+// export default defineConfig({
+//   plugins: [react()]
+// })
+
+const defaultConfig = {
+  plugins: [react()],
+};
+
+export default defineConfig(({ command, mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), '')
+
+  const apiURL = `${env.VITE_BACKEND_URL}`
+  if (command === "serve") {
+    return {
+      ...defaultConfig,
+      server: {
+        proxy: {
+          "/api":  "http://localhost:6003",
+        },
+      },
+    };
+  } else {
+    return {
+      ...defaultConfig,
+      proxy: {
+        '/api' : apiURL,
+      }
+    };
+  }
+});
