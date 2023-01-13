9a66c3c6db18f79c20a951fe5241d60a698cdd7b
vimo <vidyesh@yahoo.com>
2023-01-13 20:02:49 +0530

    rolling back changes

--- todo-app/frontend/vite.config.js
+++ todo-app/frontend/vite.config.js
@@ -1,39 +1,7 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()]
-// })
-
-const defaultConfig = {
-  plugins: [react()],
-};
-
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  const apiURL = `${env.VITE_BACKEND_URL}`;
-  if (command === "serve") {
-    return {
-      ...defaultConfig,
-      server: {
-        proxy: {
-          "/api": "http://localhost:6003",
-        },
-      },
-    };
-  } else {
-    return {
-      ...defaultConfig,
-      server: {
-        proxy: {
-          "/api": {
-            target: apiURL,
-            changeOrigin: true,
-          },
-        },
-      },
-    };
-  }
-});
+export default defineConfig({
+  plugins: [react()]
+})
