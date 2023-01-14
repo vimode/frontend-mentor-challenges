c7f0ea31577160dbd5522f945cc6d70cf3cc3508
vimo <vidyesh@yahoo.com>
2023-01-14 19:27:06 +0530

    vite basic proxy config

--- todo-app/frontend/vite.config.js
+++ todo-app/frontend/vite.config.js
@@ -8,6 +8,11 @@ import react from "@vitejs/plugin-react";
 
 const defaultConfig = {
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': "https://fm-todo-backend-production.up.railway.app"
+    }
+  }
 };
 
 export default defineConfig(({ command, mode }) => {
@@ -15,26 +20,28 @@ export default defineConfig(({ command, mode }) => {
 
   const apiURL = `https://${env.VITE_BACKEND_URL}`;
 
-  if (command === "serve") {
-    return {
-      ...defaultConfig,
-      server: {
-        proxy: {
-          "/api": "http://localhost:6003",
-        },
-      },
-    };
-  } else {
-    return {
-      ...defaultConfig,
-      server: {
-        proxy: {
-          "/api": {
-            target:'https://fm-todo-backend-production.up.railway.app',
-            changeOrigin: true,
-          },
-        },
-      },
-    };
-  }
+  return defaultConfig
+
+  // if (command === "serve") {
+  //   return {
+  //     ...defaultConfig,
+  //     server: {
+  //       proxy: {
+  //         "/api": "http://localhost:6003",
+  //       },
+  //     },
+  //   };
+  // } else {
+  //   return {
+  //     ...defaultConfig,
+  //     server: {
+  //       proxy: {
+  //         "/api": {
+  //           target:'https://fm-todo-backend-production.up.railway.app',
+  //           changeOrigin: true,
+  //         },
+  //       },
+  //     },
+  //   };
+  // }
 });
