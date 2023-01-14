94e407163364ec4effc3cfbbeeb48bb804e7fc35
vimo <vidyesh@yahoo.com>
2023-01-14 19:33:36 +0530

    roll back vite config changes

--- todo-app/frontend/vite.config.js
+++ todo-app/frontend/vite.config.js
@@ -8,40 +8,33 @@ import react from "@vitejs/plugin-react";
 
 const defaultConfig = {
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': "https://fm-todo-backend-production.up.railway.app"
-    }
-  }
 };
 
 export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
-  const apiURL = `https://${env.VITE_BACKEND_URL}`;
+  const apiURL = `'https://${env.VITE_BACKEND_URL}'`;
 
-  return defaultConfig
-
-  // if (command === "serve") {
-  //   return {
-  //     ...defaultConfig,
-  //     server: {
-  //       proxy: {
-  //         "/api": "http://localhost:6003",
-  //       },
-  //     },
-  //   };
-  // } else {
-  //   return {
-  //     ...defaultConfig,
-  //     server: {
-  //       proxy: {
-  //         "/api": {
-  //           target:'https://fm-todo-backend-production.up.railway.app',
-  //           changeOrigin: true,
-  //         },
-  //       },
-  //     },
-  //   };
-  // }
+  if (command === "serve") {
+    return {
+      ...defaultConfig,
+      server: {
+        proxy: {
+          "/api": "http://localhost:6003",
+        },
+      },
+    };
+  } else {
+    return {
+      ...defaultConfig,
+      server: {
+        proxy: {
+          "/api": {
+            target: apiURL,
+            changeOrigin: true,
+          },
+        },
+      },
+    };
+  }
 });
