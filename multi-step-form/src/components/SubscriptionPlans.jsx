7c70aa9ec284873f7e81fc159b47595fc4c165b8
vimo <39148877+vimode@users.noreply.github.com>
2023-05-15 08:40:52 +0530

    Add classname and update structure

--- multi-step-form/src/components/SubscriptionPlans.jsx
+++ multi-step-form/src/components/SubscriptionPlans.jsx
@@ -1,5 +1,6 @@
 import { useLocalStorageData } from "../useLocalStorageData";
 import {plans as plansData } from "../data";
+import "./_subscriptionplans.scss";
 
 function SubscriptionPlans() {
   const [thisFormData, setThisFormData] = useLocalStorageData("localFormData");
@@ -29,22 +30,27 @@ function SubscriptionPlans() {
     <>
       <h1>Select your plan</h1>
       <p>You have the option of monthly or annual billing</p>
-      <form>
-        { plansData.map(plan => {
-          return (
-          <div key={plan.id}>
-            <label htmlFor={plan.name}>{plan.name}</label>
-            <input type="radio" 
-                id={plan.name} 
-                name={plan.name} 
-                value={plan.name} 
-                onChange={handleTypeChange} 
-                checked={thisFormData.plan.type === plan.name}/>
-            <p>{thisFormData.plan.period ? `$${plan.yearly}/yr` : `$${plan.monthly}/mo`}</p>
-            {thisFormData.plan.period && <p>2 months free</p>}
-          </div> )
-          })
-        }
+      <form className="subscriptionForm">
+        <div className="planOptions">
+          { plansData.map(plan => {
+            return (
+              <div className="planCard" key={plan.id}>
+                <img className="iconImage" src="images/icon-pro.svg"/>
+                <input type="radio" 
+                    id={plan.name} 
+                    name={plan.name} 
+                    value={plan.name} 
+                    onChange={handleTypeChange} 
+                    checked={thisFormData.plan.type === plan.name}/>
+                <div>
+                  <label htmlFor={plan.name}>{plan.name}</label>
+                  <p>{thisFormData.plan.period ? `$${plan.yearly}/yr` : `$${plan.monthly}/mo`}</p>
+                  {thisFormData.plan.period && <p className="promoText">2 months free</p>}
+                </div>
+              </div>)
+            })
+          }
+        </div>
         <div>
           <label htmlFor="period">Monthly</label>
           <input
