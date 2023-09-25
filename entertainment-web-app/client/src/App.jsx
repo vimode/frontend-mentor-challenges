a6f1688c204213de5f9e669a6a9160e41a410285
vimo <39148877+vimode@users.noreply.github.com>
2023-09-25 20:34:58 +0530

    Add ErrorElement to some routes

--- entertainment-web-app/client/src/App.jsx
+++ entertainment-web-app/client/src/App.jsx
@@ -42,6 +42,7 @@ const router = createBrowserRouter(
           loader={({ params }) => {
             return getMovieDetails(params.id);
           }}
+          errorElement={<Error />}
         />
         <Route
           path="tvshows"
@@ -55,6 +56,7 @@ const router = createBrowserRouter(
           loader={({ params }) => {
             return getTVShowDetails(params.id);
           }}
+          errorElement={<Error />}
         />
         <Route
           path="bookmarks"
