28e8705eff93c33a8311b2aed1bb8be8c4072bb3
vimo <39148877+vimode@users.noreply.github.com>
2023-12-08 17:48:37 +0530

    dictionary-web-app: update fetch calls

--- dictionary-web-app/src/App.tsx
+++ dictionary-web-app/src/App.tsx
@@ -12,31 +12,59 @@ function Header () {
   )
 }
 
-function App() {
-  const [ value, setValue] = useState('')
-  const [ debouncedSearchValue, setDebouncedSearchValue] = useState('')
-  const [definitions, setdefinitions] = useState([])
+function NotFound ({notFound, word}) {
+  return (
+    <>
+      <h1>{notFound.title} for {word}</h1>
+      <p>{notFound.message}</p>
+      <p>{notFound.resolution}</p>
+    </>
+  )
+}
 
-  async function getDefinition (word:string) {
-    const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
-    const data = await fetch(url)
-    const response = await data.json()
-    setdefinitions(response)
-  }
+function App() {
+  const [ value, setValue ] = useState('')
+  const [ debouncedSearchValue, setDebouncedSearchValue ] = useState('')
+  const [ definitions, setDefinitions ] = useState([])
+  const [ notFound, setNotFound ] = useState({})
+  const [ error, setError ] = useState(null)
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       setDebouncedSearchValue(value.toLowerCase().trim());
     },500);
     return () => {
-      clearTimeout(timeout)
-    }
+      clearTimeout(timeout)    
+    }  
   },[value])
 
   useEffect(() => {
-    getDefinition(debouncedSearchValue)
+    if(debouncedSearchValue.length){
+      (async () => {
+        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${debouncedSearchValue}`
+        try {
+          const response = await fetch(url);
+          if(response.status === 404) {
+            const data = await response.json()
+            setNotFound(data)
+            setError(null)
+            setDefinitions([])
+          }
+          if(response.status === 200) {
+            const data = await response.json()
+            setDefinitions(data)
+            setNotFound({})
+            setError(null)
+          }
+        }catch(error){
+            setError(error.message);
+            setDefinitions([])
+            setNotFound({})
+          }
+      })();
+    }
   },[debouncedSearchValue])
- 
+
   return (
     <>
       <Header />
@@ -46,14 +74,16 @@ function App() {
             <input 
               type="search"
               value={value}
-              onChange = {(e) =>setValue(e.target.value)}
+              onChange = {(e) => setValue(e.target.value)}
               placeholder= "Search for any word..."
             />
             <img src="/images/icon-search.svg" alt="search" />
           </label>
         </header>
+        {notFound.message && <NotFound notFound={notFound} word={debouncedSearchValue}/>}
+        {error && <div>There was a problem fetching definition : {error}</div>}
         <ul>
-        {definitions.length > 0 && 
+        {definitions && definitions.length > 0 && 
           definitions.map((d)=> 
             <li>
               <div>
