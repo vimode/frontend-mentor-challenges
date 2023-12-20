c55ce296731c608741e8cc89eba9147d11b3b978
vimo <39148877+vimode@users.noreply.github.com>
2023-12-20 17:25:04 +0530

    dictionary-web-app : Create utils

---
+++ dictionary-web-app/src/utils/common.utils.ts
@@ -0,0 +1,26 @@
+import { Phonetics } from "../types";
+
+export function phoneticAudioFinder(phonetics: Phonetics[]): {
+  text: string;
+  audio?: HTMLAudioElement | null;
+} {
+  const valid = phonetics.filter((p: Phonetics) => p.audio);
+  const audioTrack = valid.length > 0 ? new Audio(valid[0].audio) : null;
+  return {
+    text: valid.length > 0 ? valid[0].text : phonetics[0].text,
+    audio: audioTrack || null,
+  };
+}
+
+export function audioControl(audioTrack) : void {
+  if (!audioTrack.playing) {
+    audioTrack.play();
+  } else {
+    audioTrack.pause();
+  }
+}
+
+// for validation
+export function isEmpty(inputString:string) : boolean{
+  return !inputString.trim().length;
+}
