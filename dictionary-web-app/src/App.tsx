611824ff05b8d49f999e9018a861f3b8670432f0
vimo <39148877+vimode@users.noreply.github.com>
2023-12-20 17:26:08 +0530

    dictionary-web-app : Update style and formatting

--- dictionary-web-app/src/App.tsx
+++ dictionary-web-app/src/App.tsx
@@ -3,114 +3,142 @@ import Header from "./components/Header";
 import NotFound from "./components/NotFound";
 import { ThemeContext } from "./context/themeContext";
 import { FontContext } from "./context/fontContext";
-import { Definition, NotFoundError, Phonetics } from "./types";
+import { Definition, NotFoundError } from "./types";
+import { phoneticAudioFinder, audioControl } from "./utils/common.utils";
 
 function App() {
-  const [ value, setValue ] = useState('')
-  const [ debouncedSearchValue, setDebouncedSearchValue ] = useState('')
-  const [ definitions, setDefinitions ] = useState<Definition [] |undefined>([]);
-  const [ notFound, setNotFound ] = useState<NotFoundError | null>(null)
-  const [ error, setError ] = useState(null);
-  const { theme }= useContext(ThemeContext)
-  const { font } = useContext(FontContext)
+  const [value, setValue] = useState("");
+  const [debouncedSearchValue, setDebouncedSearchValue] = useState("");
+  const [definitions, setDefinitions] = useState<Definition[] | undefined>([]);
+  const [notFound, setNotFound] = useState<NotFoundError | null>(null);
+  const [isValidInput, setIsValidInput] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const { theme } = useContext(ThemeContext);
+  const { font } = useContext(FontContext);
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       setDebouncedSearchValue(value.toLowerCase().trim());
-    },500);
+    }, 500);
     return () => {
-      clearTimeout(timeout)
-    }
-  },[value])
+      clearTimeout(timeout);
+    };
+  }, [value]);
 
   useEffect(() => {
-    if(debouncedSearchValue.length === 0) {
-      setNotFound(null)
-      setError(null)
-      setDefinitions([])
+    if (debouncedSearchValue.length === 0) {
+      setNotFound(null);
+      setError(null);
+      setDefinitions([]);
     }
-    if(debouncedSearchValue.length > 0){
+    if (debouncedSearchValue.length > 0) {
       (async () => {
-        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${debouncedSearchValue}`
+        setIsLoading(true);
+        const url = `https://api.dictionaryapi.dev/api/v2/entries/en/${debouncedSearchValue}`;
         try {
           const response = await fetch(url);
-          if(response.status === 404) {
-            const data = await response.json()
-            setNotFound(data)
-            setError(null)
-            setDefinitions([])
+          if (response.status === 404) {
+            const data = await response.json();
+            setNotFound(data);
+            setError(null);
+            setDefinitions([]);
           }
-          if(response.status === 200) {
-            const data = await response.json()
-            setDefinitions(data)
-            setNotFound(null)
-            setError(null)
+          if (response.status === 200) {
+            const data = await response.json();
+            setDefinitions(data);
+            setNotFound(null);
+            setError(null);
           }
-        }catch(error){
-            setError(error);
-            setDefinitions([])
-            setNotFound(null)
-          }
+        } catch (error) {
+          setError(error);
+          setDefinitions([]);
+          setNotFound(null);
+        } finally {
+          setIsLoading(false);
+        }
       })();
     }
-  },[debouncedSearchValue])
-
-  function phoneticFAudioFinder(phonetics:Phonetics []) {
-    const valid = phonetics.filter((p:Phonetics) => p.text && p.audio)
-    const audioTrack = valid.length > 0 ? new Audio(valid[0].audio) : null;
-    return {
-      text: valid.length > 0 ? valid[0].text :  phonetics[0].text,
-      audio: audioTrack || null,
-    }
-  }
-
-  function audioControl (audioTrack) {
-    if(!audioTrack.playing) {
-      audioTrack.play()
-    } else {
-      audioTrack.pause()
-    }
-  }
-
+  }, [debouncedSearchValue]);
 
   return (
-        <div className={`main_wrapper ${theme ? "switchTheme" : ""} font${font}`}>
-          <Header/>
-          <main id="main-content">
-            <header>
-              <label>
-                  <input 
-                    type="search"
-                    value={value}
-                    onChange = {(e) => setValue(e.target.value)}
-                    placeholder= "Search for any word..."
-                  />
-                  <img src="/images/icon-search.svg" alt="search" className="searchIcon"/>
-              </label>
-            </header>
-            {notFound?.message && <NotFound notFound={notFound} word={debouncedSearchValue}/>}
-            {error && <div>There was a problem fetching definition : {error}</div>}
-            <div className="inner_wrapper">
-            {definitions && definitions.length > 0 ? 
-              definitions.map((d)=> 
-                <>
+    <div className={`main_wrapper ${theme ? "switchTheme" : ""} font${font}`}>
+      <Header />
+      <main id="main-content">
+        <form onSubmit = { (e) => console.log(e)}>
+          <label>
+            <input
+              type="search"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+              placeholder="Search for any word..."
+            />
+              <img
+                src="/images/icon-search.svg"
+                alt="search"
+                className="searchIcon"
+            />
+          </label>
+        </form>
+        {(isLoading && !definitions?.length > 0) ? (
+          <div className="loader"></div>
+        ) : null}
+        {notFound?.message && (
+          <NotFound notFound={notFound} word={debouncedSearchValue} />
+        )}
+        {error && <div>There was a problem fetching definition : {error}</div>}
+        <div className="inner_wrapper">
+          {definitions && definitions.length > 0 ? (
+            definitions.map((d) => (
+              <div className="item_wrapper">
                 <header>
                   <h1>{d.word}</h1>
-                  {d.phonetics.length > 0 ?
-                      <>
-                      <p>{phoneticFAudioFinder(d.phonetics).text}</p>
-                      {phoneticFAudioFinder(d.phonetics).audio !== null ?  
+                  {d.phonetics.length > 0 ? (
+                    <>
+                      <p>{phoneticAudioFinder(d.phonetics).text}</p>
+                      {phoneticAudioFinder(d.phonetics).audio !== null ? (
                         <>
-                        <button onClick={() => audioControl(phoneticFAudioFinder(d.phonetics).audio)}> 
-                          <svg xmlns="http://www.w3.org/2000/svg" width="75" height="75" viewBox="0 0 75 75"><g fill="#A445ED" fillRule="evenodd"><circle cx="37.5" cy="37.5" r="37.5" opacity=".25"/><path d="M29 27v21l21-10.5z"/></g></svg>
-                        </button>
-                        <audio>
-                          <source src={phoneticFAudioFinder(d.phonetics).audio}/>
-                        </audio>
-                       </> : null }
-                    </> : <></> }
+                          <button
+                            onClick={() =>
+                              audioControl(
+                                phoneticAudioFinder(d.phonetics).audio
+                              )
+                            }
+                          >
+                            {isLoading ? (
+                              <div className="loader"></div>
+                            ) : (
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="75"
+                                height="75"
+                                viewBox="0 0 75 75"
+                              >
+                                <g fill="#A445ED" fillRule="evenodd">
+                                  <circle
+                                    cx="37.5"
+                                    cy="37.5"
+                                    r="37.5"
+                                    opacity=".25"
+                                  />
+                                  <path d="M29 27v21l21-10.5z" />
+                                </g>
+                              </svg>
+                            )}
+                          </button>
+                          <audio>
+                            <source
+                              src={phoneticAudioFinder(d.phonetics).audio}
+                            />
+                          </audio>
+                        </>
+                      ) : null}
+                    </>
+                  ) : (
+                    <></>
+                  )}
                 </header>
-                {d.meanings.map((dm) => 
+                {d.meanings.map((dm) => (
                   <ul>
                     <div className="innerTitle_wrapper">
                       <h2>{dm.partOfSpeech}</h2>
@@ -118,33 +146,42 @@ function App() {
                     </div>
                     <h3>Meaning</h3>
                     <ul className="list_style">
-                      {dm.definitions.map((dmd) => 
+                      {dm.definitions.map((dmd) => (
                         <>
                           <li>{dmd.definition}</li>
-                          {dmd.example && <p>{dmd.example}</p> }
-                      </>
-                      )}
+                          {dmd.example && <p>{dmd.example}</p>}
+                        </>
+                      ))}
                     </ul>
-                    {dm.synonyms.length > 0 && 
+                    {dm.synonyms.length > 0 && (
                       <div className="inline_content">
-                        <h3 className="inline_heading">Synonyms &nbsp; &nbsp;</h3>
-                          <ul className="inline_list">
-                            {dm.synonyms.map((dmss) => 
+                        <h3 className="inline_heading">
+                          Synonyms &nbsp; &nbsp;
+                        </h3>
+                        <ul className="inline_list">
+                          {dm.synonyms.map((dmss) => (
                             <li>{dmss}</li>
-                            )}
-                          </ul>
+                          ))}
+                        </ul>
                       </div>
-                    }
+                    )}
                   </ul>
-                )}
-              <p>Source &nbsp; &nbsp; <a href={d.sourceUrls[0]}>{d.sourceUrls[0]}</a></p>
-                </>
-              )
-            :<></>}
-            </div>
-          </main>
+                ))}
+                <hr />
+                <div className="footer_text">
+                  <p>Source</p>&nbsp; &nbsp;
+                  <a href={d.sourceUrls[0]}>{d.sourceUrls[0]}</a>&nbsp;
+                  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14"><path fill="none" stroke="#838383" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M6.09 3.545H2.456A1.455 1.455 0 0 0 1 5v6.545A1.455 1.455 0 0 0 2.455 13H9a1.455 1.455 0 0 0 1.455-1.455V7.91m-5.091.727 7.272-7.272m0 0H9m3.636 0V5"/></svg>
+                </div>
+              </div>
+            ))
+          ) : (
+            <></>
+          )}
         </div>
-  )
+      </main>
+    </div>
+  );
 }
 
-export default App
+export default App;
