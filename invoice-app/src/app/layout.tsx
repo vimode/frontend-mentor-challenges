a364eafd56ace0b6d89fa04cf0510f179d358137
vimo <39148877+vimode@users.noreply.github.com>
2024-06-15 17:04:06 +0530

    Fix favicon error

--- invoice-app/src/app/layout.tsx
+++ invoice-app/src/app/layout.tsx
@@ -22,19 +22,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-				<link rel="icon" href="/favicon1.ico" sizes="any" />
-				<link
-					rel="icon"
-					href="/icon?<generated>"
-					type="image/<generated>"
-					sizes="<generated>"
-				/>
-				<link
-					rel="apple-touch-icon"
-					href="/apple-icon?<generated>"
-					type="image/<generated>"
-					sizes="<generated>"
-				/>
+				<link rel="icon" href="/favicon.ico" sizes="any" />
 			</head>
 			<body className={spartan.className}>{children}</body>
 		</html>
