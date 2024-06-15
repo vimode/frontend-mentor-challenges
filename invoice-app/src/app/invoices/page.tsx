91b3c637dbffc78fd56ca021fb7fb28efe5a1f3c
vimo <39148877+vimode@users.noreply.github.com>
2024-06-15 17:04:21 +0530

    Update invoices route

--- invoice-app/src/app/invoices/page.tsx
+++ invoice-app/src/app/invoices/page.tsx
@@ -1,3 +1,36 @@
+import data from "@/data.json";
+
 export default function Invoices() {
-	return <div>Invoices go here</div>;
+	return (
+		<section>
+			<div>
+				<div>
+					<h1>Invoices</h1>
+					<p>There are 7 total invoices</p>
+				</div>
+				<div>
+					<p>Filter</p>
+					<button>New Invoice</button>
+				</div>
+			</div>
+			{data ? (
+				data.map((invoice) => (
+					<ul key={invoice.id}>
+						<li>
+							<p>{invoice.id}</p>
+							<p>{invoice.paymentDue}</p>
+							<p>{invoice.clientName}</p>
+							<p>{invoice.total}</p>
+							<p>{invoice.status}</p>
+							<p>
+								<a href={`/invoices/${invoice.id}`}>&gt;</a>
+							</p>
+						</li>
+					</ul>
+				))
+			) : (
+				<div>Nothing</div>
+			)}
+		</section>
+	);
 }
