194c4e48bc8a68de2c034b7523621f9a4c83e081
vimo <39148877+vimode@users.noreply.github.com>
2024-08-28 16:55:57 +0530

    feat: return dummy content

--- invoice-app-v2/src/app/invoices/page.tsx
+++ invoice-app-v2/src/app/invoices/page.tsx
@@ -1,3 +1,33 @@
 export default function Invoices() {
-	return <h1>Invoices</h1>;
+	return (
+		<div>
+			<div>Sidebar</div>
+			<main>
+				<div>
+					<div>
+						<h1>Invoices</h1>
+						<p>There are 7 total invoices</p>
+					</div>
+					<div>
+						<div>
+							<p>Filter</p>
+							<button>New Invoice</button>
+						</div>
+					</div>
+				</div>
+				<section>
+					<ul>
+						<li>
+							<p>#Invoice id</p>
+							<p>Invoice Date</p>
+							<p>Client Name</p>
+							<p>Invoice Total</p>
+							<p>Invoice Status</p>
+							<a href="#">{`>`}</a>
+						</li>
+					</ul>
+				</section>
+			</main>
+		</div>
+	);
 }
