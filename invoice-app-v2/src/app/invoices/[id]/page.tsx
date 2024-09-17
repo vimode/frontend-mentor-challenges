e621f2720abb917add44b8293455e9ae5252a123
vimo <39148877+vimode@users.noreply.github.com>
2024-09-17 17:34:54 +0530

    chore:CSS and dynamic route

--- invoice-app-v2/src/app/invoices/[id]/page.tsx
+++ invoice-app-v2/src/app/invoices/[id]/page.tsx
@@ -1,3 +1,12 @@
-export default function InvoiceId({ params }: { params: { id: string } }) {
-	return <>Invoice id {params.id}</>;
+export default async function InvoiceId({
+	params,
+}: { params: { id: string } }) {
+	const res = await fetch(`${process.env.API_URL}/invoices/api`);
+	const data = await res.json();
+	const invoice = data.data.filter(
+		(item) => item.id.toString() === params.id.toString(),
+	)[0];
+	console.log(invoice);
+
+	return <>Invoice id: {invoice.id}</>;
 }
