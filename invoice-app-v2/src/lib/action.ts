01fa8895be60af1c0d0ae0bc5fb974ea09ea1697
vimo <39148877+vimode@users.noreply.github.com>
2024-09-18 17:54:36 +0530

    feat: add invoice status update toggle"

--- invoice-app-v2/src/lib/action.ts
+++ invoice-app-v2/src/lib/action.ts
@@ -1,13 +1,41 @@
+"use server";
 import Invoice from "@/models/invoice";
 import { connectToMongoDB } from "./dbConnect";
 
-export const allInvoices = async () => {
+export async function allInvoices() {
 	try {
 		await connectToMongoDB();
 		const invoices = await Invoice.find({});
 		return invoices;
 	} catch (error) {
 		console.error("Error fetching invoices:", error);
-		throw error;
+		return Response.json({
+			status: "error",
+			message: "Failed to get invoices",
+		});
 	}
-};
+}
+
+export async function updateInvoiceStatus(data) {
+	const { id } = data;
+	const filter = { id };
+	const update = { status: "paid" };
+
+	try {
+		await connectToMongoDB();
+		const updated = await Invoice.findOneAndUpdate(filter, update, {
+			new: true,
+		});
+		console.log(updated);
+	} catch (error) {
+		console.error("Error updating invoice status:", error);
+		return Response.json(
+			{ status: "error", message: "Failed to update invoice status" },
+			{ status: 500 },
+		);
+	}
+}
+
+export async function createNewInvoice(data) {
+	return Response.json({ status: "pending action" });
+}
