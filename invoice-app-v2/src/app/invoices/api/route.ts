1ebbd04451fc7325d4940a5275ae16565f6e3497
vimo <39148877+vimode@users.noreply.github.com>
2024-09-18 17:53:47 +0530

    feat: add update invoicestatus action

--- invoice-app-v2/src/app/invoices/api/route.ts
+++ invoice-app-v2/src/app/invoices/api/route.ts
@@ -1,7 +1,26 @@
-import { allInvoices } from "@/lib/action";
+import {
+	allInvoices,
+	updateInvoiceStatus,
+	createNewInvoice,
+} from "@/lib/action";
+
+export const dynamic = "force-static";
 
 export async function GET() {
 	let data = await allInvoices();
 
 	return Response.json({ data });
+}
+
+export async function POST(request) {
+	const [action, data] = await request.json();
+
+	switch (action) {
+		case "updateInvoiceStatus":
+			return await updateInvoiceStatus(data);
+		case "createNewInvoice":
+			return await createNewInvoice(data);
+		default:
+			return Response.json({ error: "Invalid cction" }, { status: 400 });
+	}
 }
