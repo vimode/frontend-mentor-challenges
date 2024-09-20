ef54bf3a2289027bbaf14043fc93b2d72f181578
vimo <39148877+vimode@users.noreply.github.com>
2024-09-20 17:53:31 +0530

    chore: update styling for sidebar height

--- invoice-app-v2/src/app/invoices/layout.tsx
+++ invoice-app-v2/src/app/invoices/layout.tsx
@@ -2,8 +2,8 @@ export default function InvoicesLayout({
 	children,
 }: { children: React.ReactNode }) {
 	return (
-		<main className="flex flex-row w-screen h-full">
-			<section className="h-full max-w-[100px] rounded-r-lg bg-status-neutral">
+		<main className="flex flex-row w-screen min-h-screen">
+			<section className="min-h-screen max-w-[100px] rounded-r-lg bg-status-neutral">
 				<div className="flex flex-col items-center h-full place-content-between">
 					<p>T</p>
 					<img src="/assets/image-avatar.jpg" className="rounded-full w-1/2" />
