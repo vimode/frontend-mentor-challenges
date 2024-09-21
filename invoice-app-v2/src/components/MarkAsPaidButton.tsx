974e002966cd7ff63131c1121b47f399540c208f
vimo <39148877+vimode@users.noreply.github.com>
2024-09-21 17:28:55 +0530

    test: update invoice page test to revalidate and reload

--- invoice-app-v2/src/components/MarkAsPaidButton.tsx
+++ invoice-app-v2/src/components/MarkAsPaidButton.tsx
@@ -1,12 +1,21 @@
 "use client";
 import { updateInvoiceStatus } from "@/lib/action";
 import { InvoiceDetails } from "@/lib/types";
+import { revalidatePath } from "next/cache";
+import { useRouter } from "next/navigation";
 
-export default function MarkAsPaidButton({ invoice }: { invoice: InvoiceDetails }) {
+export default function MarkAsPaidButton({
+	invoice,
+}: { invoice: InvoiceDetails }) {
+	const router = useRouter();
 	return (
 		<button
-			onClick={() => {
-				updateInvoiceStatus({ id: invoice.id });
+			onClick={async () => {
+				const response = await updateInvoiceStatus({ id: invoice.id });
+				// if (response.status === "success") {
+				// 	window.location.reload();
+				// 	revalidatePath(`/invoices/${invoice.id}`);
+				// }
 			}}
 		>
 			Mark as Paid
