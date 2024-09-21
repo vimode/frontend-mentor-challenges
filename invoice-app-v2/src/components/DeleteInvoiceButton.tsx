57babb0c0e54077bd3c48c4cf6c2e501e06474f4
vimo <39148877+vimode@users.noreply.github.com>
2024-09-21 17:28:13 +0530

    feat: page redirect /invoices when /[id] deleted

--- invoice-app-v2/src/components/DeleteInvoiceButton.tsx
+++ invoice-app-v2/src/components/DeleteInvoiceButton.tsx
@@ -1,15 +1,19 @@
 "use client";
 import { deleteInvoice } from "@/lib/action";
 import { InvoiceDetails } from "@/lib/types";
+import { useRouter } from "next/navigation";
 
 export default function MarkAsPaidButton({
 	invoice,
 }: { invoice: InvoiceDetails }) {
+	const router = useRouter();
+
 	return (
 		<button
 			className="bg-background-alert text-white py-2 px-4 rounded-full"
 			onClick={() => {
 				deleteInvoice({ id: invoice.id });
+				router.push("/invoices");
 			}}
 		>
 			Delete
