d24ab8a5e9d3f830c7fd41b19610bfd951a52df9
vimo <39148877+vimode@users.noreply.github.com>
2024-09-26 18:50:56 +0530

    feat: add invoice id generator

--- invoice-app-v2/src/lib/helper.ts
+++ invoice-app-v2/src/lib/helper.ts
@@ -1,4 +1,4 @@
-export function formatDate(dbdate: Date) {
+export function formatDate(dbdate: Date): string {
 	const date = new Date(dbdate);
 	const yyyy = date.getFullYear();
 	const mm = String(date.getMonth() + 1).padStart(2, "0");
@@ -6,10 +6,21 @@ export function formatDate(dbdate: Date) {
 	return `${yyyy}-${mm}-${dd}`;
 }
 
-export function formatDateMedFormat(dbdate: Date) {
+export function formatDateMedFormat(dbdate: Date): string {
 	const date = new Date(dbdate);
 	const yyyy = date.getFullYear();
 	const mm = new Intl.DateTimeFormat("en-US", { month: "short" }).format(date);
 	const dd = String(date.getDate()).padStart(2, "0");
 	return `${dd} ${mm} ${yyyy}`;
+}
+
+export function generateInvoiceNumber(): string {
+	const letters = String.fromCharCode(
+		...Array.from({ length: 2 }, () => Math.floor(Math.random() * 26) + 65),
+	);
+	const numbers = Array.from({ length: 4 }, () =>
+		Math.floor(Math.random() * 10),
+	).join("");
+	const invoiceNumber = letters + numbers;
+	return invoiceNumber;
 }
