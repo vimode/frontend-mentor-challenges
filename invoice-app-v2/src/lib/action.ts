1e89abe66cc3e238472c1cb7f3e66e68fe921b8e
vimo <39148877+vimode@users.noreply.github.com>
2024-09-30 18:14:31 +0530

    update: add revalidate to clear cache and update action function logic for status

--- invoice-app-v2/src/lib/action.ts
+++ invoice-app-v2/src/lib/action.ts
@@ -2,6 +2,7 @@
 import Invoice from "@/models/invoice";
 import { connectToMongoDB } from "./dbConnect";
 import { InvoiceDetails } from "./types";
+import { revalidatePath } from "next/cache";
 
 // Get all invoices
 export async function allInvoices() {
@@ -19,16 +20,28 @@ export async function allInvoices() {
 }
 
 // Mark invoice as paid
-export async function updateInvoiceStatus(data) {
-	const { id } = data;
+export async function updateInvoiceStatus(data: {
+	id: string;
+	status: string;
+}) {
+	const { id, status } = data;
 	const filter = { id };
-	const update = { status: "paid" };
+	const updateToPaid = { status: "paid" };
+	const updateToPending = { status: "pending" };
 
 	try {
 		await connectToMongoDB();
-		const updated = await Invoice.findOneAndUpdate(filter, update, {
-			new: true,
-		});
+		let updated;
+		if (status === "paid") {
+			updated = await Invoice.findOneAndUpdate(filter, updateToPending, {
+				new: true,
+			});
+		} else {
+			updated = await Invoice.findOneAndUpdate(filter, updateToPaid, {
+				new: true,
+			});
+		}
+		revalidatePath(`/invoices`);
 		console.log(updated);
 		return {
 			status: "success",
@@ -36,40 +49,42 @@ export async function updateInvoiceStatus(data) {
 		};
 	} catch (error) {
 		console.error("Error updating invoice status:", error);
-    return Response.json(
-      { status: "error", message: "Failed to update invoice status" },
-      { status: 500 },
-      );
-    }
-  }
+		return Response.json(
+			{ status: "error", message: "Failed to update invoice status" },
+			{ status: 500 },
+		);
+	}
+}
 
-  // Delete a single invoice
-  export async function deleteInvoice(data: { id: string }) {
-    const { id } = data;
-    try {
-      await connectToMongoDB();
-      const deletedInvoice = await Invoice.deleteOne({ id });
-      console.log(deletedInvoice);
-    } catch (error) {
-      console.error("Error deleting invoice: ", error);
-      return Response.json(
-        { status: "error", message: "Failed to delete invoice" },
-        { status: 500 },
-      );
-    }
-  }
+// Delete a single invoice
+export async function deleteInvoice(data: { id: string }) {
+	const { id } = data;
+	try {
+		await connectToMongoDB();
+		const deletedInvoice = await Invoice.deleteOne({ id });
+		revalidatePath("/invoices");
+		console.log(deletedInvoice);
+	} catch (error) {
+		console.error("Error deleting invoice: ", error);
+		return Response.json(
+			{ status: "error", message: "Failed to delete invoice" },
+			{ status: 500 },
+		);
+	}
+}
 
-  // Create a new Invoice
-  export async function createNewInvoice(newInvoiceData: InvoiceDetails) {
-    try {
-    await connectToMongoDB();
-      const newInvoice = await Invoice.create(newInvoiceData);
-      console.log(`New invoice created:`, newInvoice);
-    } catch (error) {
-      console.error(`Error creating new Invoice`, error);
-      return Response.json(
-        { status: "error", message: "Failed to create invoice" },
-        { status: 500 },
-      );
-    }
-  }
+// Create a new Invoice
+export async function createNewInvoice(newInvoiceData: InvoiceDetails) {
+	try {
+		await connectToMongoDB();
+		const newInvoice = await Invoice.create(newInvoiceData);
+		revalidatePath("/invoices");
+		console.log(`New invoice created:`, newInvoice);
+	} catch (error) {
+		console.error(`Error creating new Invoice`, error);
+		return Response.json(
+			{ status: "error", message: "Failed to create invoice" },
+			{ status: 500 },
+		);
+	}
+}
