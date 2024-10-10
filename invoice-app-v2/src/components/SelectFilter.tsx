83be95844e5053bec3770ac9a495503ce32b2a8f
vimo <39148877+vimode@users.noreply.github.com>
2024-10-10 16:47:30 +0530

    feat: add filter component

---
+++ invoice-app-v2/src/components/SelectFilter.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function SelectFilter() {
+	const [selectedFilter, setSelectedFilter] = useState("");
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
+	const router = useRouter();
+
+	useEffect(() => {
+		const params = new URLSearchParams(searchParams.toString());
+		if (selectedFilter) {
+			params.set("filter", selectedFilter);
+		} else {
+			params.delete("filter");
+		}
+		router.push(pathname + "?" + params.toString());
+	}, [selectedFilter, pathname, router, searchParams]);
+
+	return (
+		<select
+			name="status"
+			id="status-select"
+			onChange={(e) => setSelectedFilter(e.target.value)}
+		>
+			<option value="">Filter by Status</option>
+			<option value="paid">Paid</option>
+			<option value="pending">Pending</option>
+			<option value="draft">Draft</option>
+			<option value="">Reset</option>
+		</select>
+	);
+}
