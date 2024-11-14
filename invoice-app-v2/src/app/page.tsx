eb54adc3c9e0c5b382d563722d9b3a754a42d6b5
vimo <39148877+vimode@users.noreply.github.com>
2024-11-14 17:26:27 +0530

    feat: add homepage login shell form

--- invoice-app-v2/src/app/page.tsx
+++ invoice-app-v2/src/app/page.tsx
@@ -2,14 +2,52 @@ import Link from "next/link";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col p-24 justify-between">
-			<p className="text-status-neutral bg-status-success">
-				Often the biggest challenge when working with a framework is figuring
-				out what you’re supposed to do when there’s something you need that the
-				framework doesn’t handle for you. Tailwind has been designed from the
-				ground up to be extensible and customizable, so that no matter what
-				you’re building you never feel like you’re fighting the framework.
-			</p>
+		<main className="flex min-h-screen flex-col p-24 justify-between place-items-center">
+			<form className="flex flex-col place-items-center ">
+				<fieldset className="flex flex-col gap-4 md:place-items-center">
+					<legend className="text-accent font-semibold text-secondary_text">
+						Login
+					</legend>
+					<div className="input-label-group w-full ">
+						<label htmlFor="username" className="form_label_basic">
+							Username
+						</label>
+						<input
+							type="text"
+							id="username"
+							name="username"
+							className="font-bold"
+						/>
+					</div>
+					<div className="input-label-group w-full ">
+						<label htmlFor="password" className="form_label_basic">
+							Password
+						</label>
+						<input
+							type="password"
+							id="password"
+							name="password"
+							className="font-bold"
+						/>
+					</div>
+				</fieldset>
+				<div className="flex flex-row gap-4 ">
+					<button
+						className="btn-basic bg-accent text-background-secondary w-max"
+						type="submit"
+						disabled
+					>
+						Login
+					</button>
+					<button
+						className="btn-basic bg-accent text-background-secondary w-max"
+						type="button"
+						disabled
+					>
+						Signup
+					</button>
+				</div>
+			</form>
 			<Link
 				className="text-[33px] text-accent underline text-center"
 				href="/invoices"
