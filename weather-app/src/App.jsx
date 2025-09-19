2606db5fea68e96e1ed3b41f7df6059846b114d1
vimo <39148877+vimode@users.noreply.github.com>
2025-09-19 17:04:28 +0530

    testing tailwind custom setup

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,8 +1,12 @@
 function App() {
 	return (
-		<>
-			<h1 className="text-3xl font-bold text-orange-700">Weather Now</h1>
-		</>
+		<div className="bg-midnight-neutral-900 flex-row gap-4 p-4">
+			<h1 className="text-midnight-neutral-0 text-preset-1 ">Weather Now</h1>
+			<p className="text-sun-orange-500 text-preset-5-semibold">
+				The quick brown fox jumps over the lazy dog.
+			</p>
+			<p className="text-preset-2 text-royal-blue-500">custom typography</p>
+		</div>
 	);
 }
 
