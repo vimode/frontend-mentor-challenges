1b1556f880ea49ab0180ba1b7da201e192ed10c4
vimo <39148877+vimode@users.noreply.github.com>
2025-09-22 17:18:23 +0530

    update weather component with static data

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -3,24 +3,24 @@ import Search from "./components/Search/Search.jsx";
 import WeatherPanels from "./components/Weather/WeatherPanels.jsx";
 
 function App() {
-	return (
-		<div>
-			<a
-				href="#main"
-				className="absolute left-0 text-midnight-neutral-0 underline text-xl sr-only focus:not-sr-only"
-			>
-				Skip to main
-			</a>
-			<Header />
-			<div>
-				<h1 className="text-preset-2">How's the sky looking today?</h1>
-				<main id="main">
-					<Search />
-					<WeatherPanels />
-				</main>
-			</div>
-		</div>
-	);
+  return (
+    <div className="p-4">
+      <a
+        href="#main"
+        className="absolute left-0 text-midnight-neutral-0 underline text-xl sr-only focus:not-sr-only"
+      >
+        Skip to main
+      </a>
+      <Header />
+      <div>
+        <h1 className="text-preset-2">How's the sky looking today?</h1>
+        <main id="main">
+          <Search />
+          <WeatherPanels />
+        </main>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- weather-app/src/components/Weather/WeatherPanels.jsx
+++ weather-app/src/components/Weather/WeatherPanels.jsx
@@ -1,12 +1,155 @@
 function WeatherPanels() {
-	return (
-		<>
-			<section>Current Weather</section>
-			<section>Current Weather details</section>
-			<section>Daily forecast</section>
-			<section>Hourly forecast</section>
-		</>
-	);
+  return (
+    <>
+      {/* Current Weather */}
+      <section className="border-2  border-amber-200 flex">
+        <div>
+          <p>Berlin, Germany</p>
+          <p>Tuesday, Aug 5, 2025</p>
+        </div>
+        <div>
+          <p>icon</p>
+          <p>20&#176;</p>
+        </div>
+      </section>
+
+      {/* Weather Details */}
+      <section className="border-2 border-red-800 flex">
+        <div>
+          <p>Feels like</p>
+          <p>18&#176;</p>
+        </div>
+        <div>
+          <p>Humidity</p>
+          <p>46%</p>
+        </div>
+        <div>
+          <p>Wind</p>
+          <p>14 km/h</p>
+        </div>
+        <div>
+          <p>Precipitation</p>
+          <p>0 mm</p>
+        </div>
+      </section>
+
+      {/* Daily Forecast */}
+      <section className="border-2 border-pink-500 flex">
+        <div>
+          <p>Mon</p>
+          <p>icon</p>
+          <div>
+            <p>20&#176;</p>
+            <p>14&#176;</p>
+          </div>
+        </div>
+        <div>
+          <p>Tue</p>
+          <p>icon</p>
+          <div>
+            <p>20&#176;</p>
+            <p>14&#176;</p>
+          </div>
+        </div>
+        <div>
+          <p>Wed</p>
+          <p>icon</p>
+          <div>
+            <p>20&#176;</p>
+            <p>14&#176;</p>
+          </div>
+        </div>
+        <div>
+          <p>Thu</p>
+          <p>icon</p>
+          <div>
+            <p>20&#176;</p>
+            <p>14&#176;</p>
+          </div>
+        </div>
+        <div>
+          <p>Fri</p>
+          <p>icon</p>
+          <div>
+            <p>20&#176;</p>
+            <p>14&#176;</p>
+          </div>
+        </div>
+        <div>
+          <p>Sat</p>
+          <p>icon</p>
+          <div>
+            <p>20&#176;</p>
+            <p>14&#176;</p>
+          </div>
+        </div>
+        <div>
+          <p>Sun</p>
+          <p>icon</p>
+          <div>
+            <p>20&#176;</p>
+            <p>14&#176;</p>
+          </div>
+        </div>
+      </section>
+
+      {/* Hourly Forecast */}
+      <section className="border-2 border-emerald-500">
+        <header className="flex justify-between">
+          <h3>Hourly forecast</h3>
+          <p>Dropdown</p>
+        </header>
+        <div>
+          <p>
+            icon<span>3 PM</span>
+          </p>
+          <p>20&#176;</p>
+        </div>
+        <div>
+          <p>
+            icon<span>4 PM</span>
+          </p>
+          <p>20&#176;</p>
+        </div>
+        <div>
+          <p>
+            icon<span>5 PM</span>
+          </p>
+          <p>20&#176;</p>
+        </div>
+        <div>
+          <p>
+            icon<span>6 PM</span>
+          </p>
+          <p>20&#176;</p>
+        </div>
+        <div>
+          <p>
+            icon<span>7 PM</span>
+          </p>
+          <p>20&#176;</p>
+        </div>
+        <div>
+          <p>
+            icon<span>8 PM</span>
+          </p>
+          <p>20&#176;</p>
+        </div>
+        <div>
+          <p>
+            icon<span>9 PM</span>
+          </p>
+          <p>20&#176;</p>
+        </div>
+        <div>
+          <p>
+            icon<span>10 PM</span>
+          </p>
+          <p>20&#176;</p>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default WeatherPanels;
