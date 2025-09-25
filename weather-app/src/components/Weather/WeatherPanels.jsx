3f6522d9dc79d2bcaf535fa05487022009f068d1
vimo <39148877+vimode@users.noreply.github.com>
2025-09-25 20:08:18 +0530

    add styling to main weather panel and details cards

--- weather-app/src/components/Weather/WeatherPanels.jsx
+++ weather-app/src/components/Weather/WeatherPanels.jsx
@@ -2,34 +2,38 @@ function WeatherPanels() {
   return (
     <>
       {/* Current Weather */}
-      <section className="border-2  border-amber-200 flex">
-        <div>
-          <p>Berlin, Germany</p>
-          <p>Tuesday, Aug 5, 2025</p>
+      <section className="flex flex-col gap-4 place-items-center rounded-[20px] px-6 py-10 bg-[url(/assets/images/bg-today-small.svg)] md:bg-[url(/assets/images/bg-today-large.svg)] bg-no-repeat bg-center bg-cover">
+        <div className="text-center flex flex-col gap-3 w-full @container/currentW">
+          <h2 className="text-preset-4">Berlin, Germany</h2>
+          <p className="text-preset-6 opacity-80">Tuesday, Aug 5, 2025</p>
         </div>
-        <div>
-          <p>icon</p>
-          <p>20&#176;</p>
+        <div className="flex place-items-center ">
+          <div className="@xs/currentW:w-full max-w-[120px] @xs/currentW:h-auto max-h-[120px]">
+            <img src="/assets/images/icon-sunny.webp" />
+          </div>
+          <p className="text-preset-1">20&#176;</p>
         </div>
       </section>
 
       {/* Weather Details */}
-      <section className="border-2 border-red-800 flex">
-        <div>
-          <p>Feels like</p>
-          <p>18&#176;</p>
+      <section className="flex flex-wrap gap-4">
+        <div className="detailsCard basis-(--xs-basis) md:basis-(--md-basis) lg:basis-(--lg-basis)">
+          <p className="text-preset-6 text-midnight-neutral-200">Feels like</p>
+          <p className="text-preset-3 text-midnight-neutral-0">18&#176;</p>
         </div>
-        <div>
-          <p>Humidity</p>
-          <p>46%</p>
+        <div className="detailsCard basis-(--xs-basis) md:basis-(--md-basis) lg:basis-(--lg-basis)">
+          <p className="text-preset-6 text-midnight-neutral-200">Humidity</p>
+          <p className="text-preset-3 text-midnight-neutral-0">46%</p>
         </div>
-        <div>
-          <p>Wind</p>
-          <p>14 km/h</p>
+        <div className="detailsCard basis-(--xs-basis) md:basis-(--md-basis) lg:basis-(--lg-basis)">
+          <p className="text-preset-6 text-midnight-neutral-200">Wind</p>
+          <p className="text-preset-3 text-midnight-neutral-0">14 km/h</p>
         </div>
-        <div>
-          <p>Precipitation</p>
-          <p>0 mm</p>
+        <div className="detailsCard basis-(--xs-basis) md:basis-(--md-basis) lg:basis-(--lg-basis)">
+          <p className="text-preset-6 text-midnight-neutral-200">
+            Precipitation
+          </p>
+          <p className="text-preset-3 text-midnight-neutral-0">0 mm</p>
         </div>
       </section>
 
