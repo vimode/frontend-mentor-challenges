1e33e6b2056dd9522a9336ee9e764706e24c61c9
vimo <39148877+vimode@users.noreply.github.com>
2025-09-26 22:57:51 +0530

    add focus styling to search

--- weather-app/src/components/Search/Search.jsx
+++ weather-app/src/components/Search/Search.jsx
@@ -16,12 +16,14 @@ function Search() {
           />
         </svg>
         <input
-          className="bg-midnight-neutral-800 px-6 py-4 pl-11 rounded-xl w-full text-midnight-neutral-200 placeholder-neutral-200"
+          className="bg-midnight-neutral-800 px-6 py-4 pl-11 rounded-xl w-full text-midnight-neutral-200 placeholder-neutral-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-midnight-neutral-900 focus-visible:ring-midnight-neutral-0 hover:cursor-pointer"
           type="search"
           placeholder="Search for a place..."
         />
       </div>
-      <button className="bg-royal-blue-500 px-6 py-4 rounded-xl">Search</button>
+      <button className="bg-royal-blue-500 px-6 py-4 rounded-xl focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-midnight-neutral-900 focus-visible:ring-royal-blue-500 hover:cursor-pointer hover:bg-royal-blue-700 transition">
+        Search
+      </button>
     </div>
   );
 }
