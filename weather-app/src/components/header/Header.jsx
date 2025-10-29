d8ce638635c9e6b1b623563afa1b60522ec1c42f
vimo <39148877+vimode@users.noreply.github.com>
2025-10-29 18:33:56 +0530

    feat: add units dropdown modal

--- weather-app/src/components/header/Header.jsx
+++ weather-app/src/components/header/Header.jsx
@@ -1,10 +1,42 @@
+import { createPortal } from "react-dom";
+import { useState } from "react";
+
+// Unis Dropdown Modal
+function UnitsDropdownModal({ onClose }) {
+  const [units, setUnits] = useState(null);
+
+  return (
+    <div className="absolute top-[120%] right-px p-2 bg-midnight-neutral-800 border-1 border-midnight-neutral-600 rounded-xl">
+      <h3>Switch to Imperial</h3>
+      <p>Temperature</p>
+      <p>Celsius (&#176;C)</p>
+    </div>
+  );
+}
+
 function Header() {
-	return (
-		<header className="flex justify-between items-center">
-			<img src="/assets/images/logo.svg" />
-			<div>units</div>
-		</header>
-	);
+  const [showUnitsDropdown, setShowUnitsDropdown] = useState(false);
+
+  return (
+    <header className="flex justify-between items-center relative" id="header">
+      <img src="./assets/images/logo.svg" />
+      <button
+        className="bg-midnight-neutral-800 px-4 py-3 rounded-lg"
+        onClick={() => {
+          setShowUnitsDropdown((prev) => !prev);
+        }}
+      >
+        <img className="inline" src="./assets/images/icon-units.svg" />
+        &nbsp;&nbsp;Units&nbsp;&nbsp;
+        <img className="inline" src="./assets/images/icon-dropdown.svg" />
+      </button>
+      {showUnitsDropdown &&
+        createPortal(
+          <UnitsDropdownModal onClose={() => setShowUnitsDropdown(false)} />,
+          document.getElementById("header"),
+        )}
+    </header>
+  );
 }
 
 export default Header;
