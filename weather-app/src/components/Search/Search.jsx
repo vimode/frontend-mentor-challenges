b620e429ddd51ea5253739682928341125fb9e51
vimo <39148877+vimode@users.noreply.github.com>
2025-11-03 08:02:48 +0530

    update: use global loading state, add input ref for search to remove focus on search, add search query history dropdown

--- weather-app/src/components/Search/Search.jsx
+++ weather-app/src/components/Search/Search.jsx
@@ -1,27 +1,27 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { getLocation } from "../../utils/getLocation";
 import { useWeatherDataContext } from "../../weatherContext";
 
 export default function Search() {
   const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
   const [queryData, setQueryData] = useState(null);
   const [queryHistory, setQueryHistory] = useState([]);
   const { setCurrentCity, loading } = useWeatherDataContext();
+  const inputRef = useRef(null);
 
   async function searchLocation(formData) {
     const query = formData.get("query").trim().toLowerCase();
-    setLoading(true);
     setError(null);
 
     const result = await getLocation(query);
-    setLoading(false);
 
     if (result.status === "success") {
       setQueryData(result.data);
       if (!queryHistory.some((city) => city.toLowerCase() === query)) {
         setQueryHistory((prev) => [...prev, result.data.city]);
         setCurrentCity((prev) => ({ ...prev, name: result.data.city }));
+        // Remove focus from input after searching. To not show the search history dropdown
+        inputRef.current.blur();
       }
     } else {
       setError(result);
@@ -29,29 +29,23 @@ export default function Search() {
   }
 
   return (
-    <form
-      action={searchLocation}
-      className="flex flex-col md:flex-row place-content-center gap-3 md:gap-4 lg:w-1/2 lg:m-auto"
-    >
-      <div className="relative w-full">
-        <img
-          className="absolute top-0 bottom-0 my-auto left-4"
-          src="./assets/images/icon-search.svg"
-        />
-        <input
-          className="bg-midnight-neutral-800 px-6 py-4 pl-11 rounded-xl w-full text-midnight-neutral-200 placeholder-neutral-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-midnight-neutral-900 focus-visible:ring-midnight-neutral-0 hover:cursor-pointer"
-          type="search"
-          placeholder="Search for a place..."
-          name="query"
-        />
-      </div>
-      <button
-        className="bg-royal-blue-500 px-6 py-4 rounded-xl focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-midnight-neutral-900 focus-visible:ring-royal-blue-500 hover:cursor-pointer hover:bg-royal-blue-700 transition"
-        type="submit"
+    <>
+      <form
+        action={searchLocation}
+        className="flex flex-col md:flex-row place-content-center gap-3 md:gap-4 lg:w-1/2 lg:m-auto"
       >
-        Search
-      </button>
-    </form>
+        <div className="relative w-full group/search">
+          <img
+            className="absolute top-0 bottom-0 my-auto left-4"
+            src="./assets/images/icon-search.svg"
+          />
+          <input
+            className="bg-midnight-neutral-800 px-6 py-4 pl-11 rounded-xl w-full text-midnight-neutral-200 placeholder-neutral-200 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-midnight-neutral-900 focus-visible:ring-midnight-neutral-0 hover:cursor-pointer "
+            type="search"
+            placeholder="Search for a place..."
+            name="query"
+            ref={inputRef}
+          />
           {loading && (
             <div className="absolute mt-3 bg-midnight-neutral-800 rounded-xl border-midnight-neutral-700 w-full text-preset-7 p-3 h-full content-center">
               <img
@@ -62,5 +56,28 @@ export default function Search() {
               &nbsp; Search in progress
             </div>
           )}
+          {queryHistory.length > 0 && (
+            <div className="absolute mt-3 bg-midnight-neutral-800 rounded-xl border-midnight-neutral-700 w-full text-preset-7 p-2 h-auto hidden group-focus-within/search:block">
+              <div className="flex flex-col items-start gap-1">
+                {queryHistory.map((query) => (
+                  <button
+                    key={query}
+                    className="px-2 py-3 hover:bg-midnight-neutral-700 rounded-lg border-midnight-neutral-600 w-full text-left cursor-pointer"
+                  >
+                    {query}
+                  </button>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+        <button
+          className="bg-royal-blue-500 px-6 py-4 rounded-xl focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-midnight-neutral-900 focus-visible:ring-royal-blue-500 hover:cursor-pointer hover:bg-royal-blue-700 transition"
+          type="submit"
+        >
+          Search
+        </button>
+      </form>
+    </>
   );
 }
