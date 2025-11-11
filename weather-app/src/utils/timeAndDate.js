be7f58099a1bcb21ce94aabcb19182c218575bcf
vimo <39148877+vimode@users.noreply.github.com>
2025-11-11 17:22:57 +0530

    feat: update to get all required values as obj

---
+++ weather-app/src/utils/timeAndDate.js
@@ -0,0 +1,49 @@
+// format date to get the expected day in full, month in short, date and year in full
+// add the timezone to adjust the offset from UTC time
+
+export function formatDate(currentDate, userTimeZone) {
+  currentDate = new Date(currentDate);
+
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    timeZone: userTimeZone || "UTC",
+  };
+
+  // mdn: use Intl for internationalization
+  const formattedDate = new Intl.DateTimeFormat("en-US", options).format(
+    currentDate,
+  );
+
+  return formattedDate;
+}
+
+export function getTimeAndDateValues(currentDateAndTime, userTimeZone) {
+  const date = new Date(currentDateAndTime);
+
+  const options = {
+    weekday: "long",
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+    timeZone: userTimeZone || "UTC",
+    hour: "numeric",
+    hour12: "true",
+    hourCycle: "h12",
+  };
+
+  // mdn: use Intl for internationalization
+  const parts = new Intl.DateTimeFormat("en-US", options).formatToParts(date);
+
+  const localeTokens = {};
+
+  parts
+    .filter((p) => p.type !== "literal")
+    .map(
+      (p) => (localeTokens[p.type] = (localeTokens[p.type] ?? "") + p.value),
+    );
+
+  return localeTokens;
+}
