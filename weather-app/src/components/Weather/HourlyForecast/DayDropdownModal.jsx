30f4079afdf7247a4aef74b38edb98f5f3b7a69e
vimo <39148877+vimode@users.noreply.github.com>
2025-11-29 07:37:41 +0530

    update: set selectedDay in daydropdown

--- weather-app/src/components/Weather/HourlyForecast/DayDropdownModal.jsx
+++ weather-app/src/components/Weather/HourlyForecast/DayDropdownModal.jsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 
 export default function DayDropdownModal({ onClose }) {
   // TODO: Move this to context
-  const [currentDay, setCurrentDay] = useState("Monday");
+  const [displayDay, setDisplayDay] = useState("Monday");
 
-  const days = [
+  const weekdays = [
     "Monday",
     "Tuesday",
     "Wednesday",
@@ -14,20 +14,20 @@ export default function DayDropdownModal({ onClose }) {
     "Sunday",
   ];
 
-  function updateDay(day) {
-    setCurrentDay(day);
+  function updateDay(selectedDay) {
+    setDisplayDay(selectedDay);
     onClose();
   }
 
   return (
     <div className="absolute top-[10%] right-[calc(var(--spacing)*4)] bg-midnight-neutral-800 border-1 border-midnight-neutral-600 rounded-xl w-1/2 p-2 z-10 flex flex-col gap-1 cursor-pointer">
-      {days.map((day) => (
+      {weekdays.map((weekday) => (
         <button
-          key={day}
-          className={`block w-full text-left text-preset-7 px-2 py-[10px] cursor-pointer transition-colors duration-300 hover:bg-midnight-neutral-700 rounded-xl ${currentDay === day ? "bg-midnight-neutral-700 rounded-xl" : ""}`}
-          onClick={() => updateDay(day)}
+          key={weekday}
+          className={`block w-full text-left text-preset-7 px-2 py-[10px] cursor-pointer transition-colors duration-300 hover:bg-midnight-neutral-700 rounded-xl ${weekday === displayDay ? "bg-midnight-neutral-700 rounded-xl" : ""}`}
+          onClick={() => updateDay(weekday)}
         >
-          {day}
+          {weekday}
         </button>
       ))}
     </div>
