0501c93b238158e54efc3c7b8dcdcedf7aa4e436
vimo <39148877+vimode@users.noreply.github.com>
2025-11-29 06:55:02 +0530

    feat: first working eighthoursdata for hourlyforecast

--- weather-app/src/components/Weather/HourlyForecast/HourlyForecastPanel.jsx
+++ weather-app/src/components/Weather/HourlyForecast/HourlyForecastPanel.jsx
@@ -1,9 +1,98 @@
 import { createPortal } from "react-dom";
 import { useState } from "react";
 import DayDropdownModal from "./DayDropdownModal";
+import HourlyCard from "./HourlyCard";
+import { getTimeAndDateValues } from "../../../utils/timeAndDate";
+import { weatherIcon } from "../../../utils/weatherIcon";
 
-export default function HourlyForecastPanel() {
+export default function HourlyForecastPanel({ weatherData }) {
   const [showDayDropdown, setShowDayDropdown] = useState(false);
+  console.log(`hourly`, weatherData ? weatherData : null);
+
+  function eightHoursData(weatherData) {
+    const userTZ = weatherData?.current?.timezone;
+    console.log(userTZ);
+
+    // Current time in user's tz
+    const timeNow = weatherData?.current?.time
+      ? getTimeAndDateValues(weatherData.current.time, userTZ)
+      : "Loading...";
+    console.log("timeNow", timeNow);
+
+    const targetDay = timeNow?.day;
+
+    // All hourly data
+    const hourlyTimes = weatherData?.hourly?.time ?? [];
+    const temps = weatherData?.hourly?.temperature_2m ?? [];
+    const weatherCode = weatherData?.hourly?.weather_code ?? [];
+    console.log("hourlyTimes", hourlyTimes);
+
+    // Filter time data for today
+    const todayFiltered = hourlyTimes?.filter(
+      (item) => new Date(item).getUTCDate() === Number(targetDay.trim()),
+    );
+    console.log("todayFiltered", todayFiltered);
+
+    // Matching Index where today's data starts and ends in hourlyTimes
+    const matchingIndex = todayFiltered?.map((item) =>
+      hourlyTimes.findIndex((t) => t.toString() === item.toString()),
+    );
+    console.log("indices_for_matching_date_in_timesdata", matchingIndex);
+
+    let startIdx = todayFiltered.findIndex(
+      (t) => new Date(t).getUTCHours() >= Number(timeNow.hour),
+    );
+    console.log("startIdx", startIdx);
+
+    // Keep track of matchingIndex
+    let chosenIndices = [];
+
+    if (startIdx === -1) {
+      // No future time data - use last 8 entries
+      console.group("No future time data");
+      chosenIndices = matchingIndex.slice(-8);
+      console.log("chosenIndices", chosenIndices);
+      console.groupEnd();
+    } else {
+      console.group("Future Times");
+      const futureTimes = matchingIndex.slice(startIdx, startIdx + 8);
+      console.log("futureTimes", futureTimes);
+      if (futureTimes.length === 8) {
+        chosenIndices = futureTimes;
+        console.log("indices length is 8", chosenIndices);
+      } else {
+        console.log("length incomplete");
+        const needed = 8 - futureTimes.length;
+        const pastTimes = matchingIndex.slice(
+          Math.max(0, startIdx - needed),
+          startIdx,
+        );
+        console.log("need", needed);
+        console.log("pastTimes", pastTimes);
+        chosenIndices = [...pastTimes, ...futureTimes];
+        console.log("combined indicies", chosenIndices);
+        console.groupEnd();
+      }
+    }
+
+    const chosenTimes = chosenIndices.map((i) => hourlyTimes[i]);
+    const chosenTemps = chosenIndices.map((i) => temps[i]);
+    const chosenCodes = chosenIndices.map((i) => weatherIcon( weatherCode[i]));
+    console.log({
+      time: chosenTimes,
+      temperature: chosenTemps,
+      weatherCode: chosenCodes,
+    });
+
+    return {
+      time: chosenTimes,
+      temperature: chosenTemps,
+      weatherCode: chosenCodes,
+    };
+  }
+
+  eightHoursData(weatherData);
+
   return (
     <section
       id="hourlyForecast"
@@ -29,66 +118,58 @@ export default function HourlyForecastPanel() {
   );
 }
 
-// TODO: Move to is own Component
+// TODO: Move to its own Component
 function HourlyForecastCard() {
   return (
     <>
-      <div className="hourlyCard">
-        <div>
-          <img src="/assets/images/icon-snow.webp" alt="" />
-          <p className="text-preset-5-semibold">3 PM</p>
-        </div>
-        <p className="text-present-7">126&#176;</p>
-      </div>
-      <div className="hourlyCard">
-        <div>
-          <img src="/assets/images/icon-rain.webp" alt="" />
-          <p className="text-preset-5-semibold">4 PM</p>
-        </div>
-        <p className="text-present-7">33&#176;</p>
-      </div>
-      <div className="hourlyCard">
-        <div>
-          <img src="/assets/images/icon-partly-cloudy.webp" alt="" />
-          <p className="text-preset-5-semibold">5 PM</p>
-        </div>
-        <p className="text-present-7">30&#176;</p>
-      </div>
-      <div className="hourlyCard">
-        <div>
-          <img src="/assets/images/icon-overcast.webp" alt="" />
-          <p className="text-preset-5-semibold">6 PM</p>
-        </div>
-        <p className="text-present-7">12&#176;</p>
-      </div>
-      <div className="hourlyCard">
-        <div>
-          <img src="/assets/images/icon-storm.webp" alt="" />
-          <p className="text-preset-5-semibold">7 PM</p>
-        </div>
-        <p className="text-present-7">7&#176;</p>
-      </div>
-      <div className="hourlyCard">
-        <div>
-          <img src="/assets/images/icon-fog.webp" alt="" />
-          <p className="text-preset-5-semibold">8 PM</p>
-        </div>
-        <p className="text-present-7">17&#176;</p>
-      </div>
-      <div className="hourlyCard">
-        <div>
-          <img src="/assets/images/icon-drizzle.webp" alt="" />
-          <p className="text-preset-5-semibold">9 PM</p>
-        </div>
-        <p className="text-present-7">29&#176;</p>
-      </div>
-      <div className="hourlyCard">
-        <div>
-          <img src="/assets/images/icon-sunny.webp" alt="" />
-          <p className="text-preset-5-semibold">10 PM</p>
-        </div>
-        <p className="text-present-7">24&#176;</p>
-      </div>
+      <HourlyCard
+        imgSrc="/assets/images/icon-snow.webp"
+        imgAlt=""
+        time="3 PM"
+        temp="22"
+      />
+      <HourlyCard
+        imgSrc="/assets/images/icon-snow.webp"
+        imgAlt=""
+        time="3 PM"
+        temp="22"
+      />
+      <HourlyCard
+        imgSrc="/assets/images/icon-snow.webp"
+        imgAlt=""
+        time="3 PM"
+        temp="22"
+      />
+      <HourlyCard
+        imgSrc="/assets/images/icon-snow.webp"
+        imgAlt=""
+        time="3 PM"
+        temp="22"
+      />
+      <HourlyCard
+        imgSrc="/assets/images/icon-snow.webp"
+        imgAlt=""
+        time="3 PM"
+        temp="22"
+      />
+      <HourlyCard
+        imgSrc="/assets/images/icon-snow.webp"
+        imgAlt=""
+        time="3 PM"
+        temp="22"
+      />
+      <HourlyCard
+        imgSrc="/assets/images/icon-snow.webp"
+        imgAlt=""
+        time="3 PM"
+        temp="22"
+      />
+      <HourlyCard
+        imgSrc="/assets/images/icon-snow.webp"
+        imgAlt=""
+        time="3 PM"
+        temp="22"
+      />
     </>
   );
 }
